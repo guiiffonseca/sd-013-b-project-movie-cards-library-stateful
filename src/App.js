13770f306e4013b10fc4d01980f0c0664bb9b27f
Marcos Estevao <dusdoom@gmail.com>
2021-07-27 15:45:50 -0300

    added input type text to the form

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
+      <SearchBar
+        searchText=""
+        onSearchTextChange=""
+      />
       <MovieList movies={ movies } />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <h1>teste</h1>
+        <label htmlFor="searchText" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            name="searchText"
+            onChange={ onSearchTextChange }
+          />
+        </label>
       </form>
     );
   }
 }
 
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
+};
 export default SearchBar;
