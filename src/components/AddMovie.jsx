bae1d9e47094ef383f8f155a5912efd3cc0e4276
gustavoalves23 <gustavo.alves388@gmail.com>
2021-07-27 15:29:49 -0300

    commit de teste

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
 // implement AddMovie component here
+import React from 'react';
+import Title from './Title';
+
+class AddMovie extends React.Component {
+
+  constructor() {
+      super();
+      this.handleChange = this.handleChange.bind(this);
+      this.props = {
+          title:"",
+      }
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+        [name]: value,
+    })
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form" action="">
+        <Title value={this.props.title} handleChange = {this.handleChange}/>
+      </form>
+    )
+  }
+}
+
+export default AddMovie;
