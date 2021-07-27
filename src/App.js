5e1d0c0c54250febf53a44d95bb85abe9390b0cd
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-07-27 17:29:49 -0300

    requisitos do 1 ao 5 ps: falta resolver o lint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Header />
+        <MovieLibrary />
       </div>
     );
   }
