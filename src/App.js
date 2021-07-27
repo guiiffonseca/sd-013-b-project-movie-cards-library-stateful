fe0f10117814e78ffd223c280adc611a55f9b151
Yg.Or <inygu@icloud.com>
2021-07-27 16:39:29 -0300

    requisito 5 e 6

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ function App() {
         onBookmarkedChange=""
       />
 
-      <AddMovie
-      
-      />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action'
-    };
-  }
 
   render() {
     return (
