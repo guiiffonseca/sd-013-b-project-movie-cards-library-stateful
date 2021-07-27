9f3754549dfc78a41c63f0fd44b76b7b99c549de
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-27 14:34:20 -0300

    fazendo o req 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,28 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 function SearchBar(props) {
-  const {searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = props;
+  const {
+    searchText,
+    onSearchTextChange,
+    bookmarkedOnly,
+    onBookmarkedChange,
+    selectedGenre,
+    onSelectedGenreChange,
+  } = props;
   return (
-    <form data-testid="search-bar-form" />
+    <form data-testid="search-bar-form">
+      <label data-testid="text-input-label" htmlFor="input-label">
+        Inclui o texto:
+        <input
+          id="input-label"
+          type="text"
+          value={ searchText }
+          data-testid="text-input"
+          onChange={ onSearchTextChange }
+        />
+      </label>
+      <input type="checkbox" />
+    </form>
   );
 }
 
