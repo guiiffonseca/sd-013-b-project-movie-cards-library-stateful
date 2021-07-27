8e51535ece94bf86fd533364e12fdcc91563bce7
Tenari <guilherme.tenarie@gmail.com>
2021-07-27 18:10:44 -0300

    Adiciona requisitios 12 - Cria input para rating

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class AddMovie extends Component {
   constructor() {
     super();
+    this.handleChanges = this.handleChanges.bind(this)
     this.state = {
       subtitle: '',
       title: '',
@@ -13,7 +14,14 @@ class AddMovie extends Component {
     };
   }
 
+  handleChanges({target}) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({[name]: value})
+  }
+
   render() {
+    const { rating } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title-input" data-testid="title-input-label">
@@ -31,6 +39,16 @@ class AddMovie extends Component {
         <label htmlFor="storyline-input" data-testid="storyline-input-label">
           Sinopse
           <textarea name="" data-testid="storyline-input" id="" cols="30" rows="10" />
+        </label>
+        <label htmlFor="rating-input" data-testid="rating-input-label">
+          Avaliação
+          <input
+            name="rating"
+            onChange={ this.handleChanges }
+            value={ rating }
+            type="number"
+            data-testid="rating-input"
+          />
         </label>
 
       </form>
