5dd39df8db550702b65b716b62e74e5696eb697a
gustavoalves23 <gustavo.alves388@gmail.com>
2021-07-27 18:02:49 -0300

    AddMovie completo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,32 +1,70 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Title from './Title';
+import Subtitle from './Subtitle';
+import Image from './Image';
+import Storyline from './Storyline';
+import RatingAddMovie from './RatingAddMovie';
+import Genre from './Genre';
+import AddMovieButton from './AddMovieButton';
+
+const defaultState = {
+  title: '',
+  subtitle: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
 
 class AddMovie extends React.Component {
-
   constructor() {
-      super();
-      this.handleChange = this.handleChange.bind(this);
-      this.props = {
-          title:"",
-      }
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.resetDefaultState = this.resetDefaultState.bind(this);
+    this.state = defaultState;
   }
 
   handleChange({ target }) {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-        [name]: value,
-    })
+      [name]: value,
+    });
+  }
+
+  resetDefaultState() {
+    const { onClick } = this.props;
+    onClick(this.props);
+    this.setState({
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
   }
 
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form" action="">
-        <Title value={this.props.title} handleChange = {this.handleChange}/>
+        <Title title={ title } handleChange={ this.handleChange } />
+        <Subtitle value={ subtitle } handleChange={ this.handleChange } />
+        <Image value={ imagePath } handleChange={ this.handleChange } />
+        <Storyline value={ storyline } handleChange={ this.handleChange } />
+        <RatingAddMovie value={ rating } handleChange={ this.handleChange } />
+        <Genre value={ genre } handleChange={ this.handleChange } />
+        <AddMovieButton onClick={ this.resetDefaultState } />
       </form>
-    )
+    );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
