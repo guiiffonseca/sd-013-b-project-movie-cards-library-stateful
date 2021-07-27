8047e17499c446def4a1a5dc8338d6d71b584a28
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-27 15:12:32 -0300

    Requisito 5 - Select and options

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -27,6 +27,29 @@ class SearchBar extends React.Component {
               data-testid="checkbox-input"
             />
           </label>
+
+          <label htmlFor="myselect" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              id="myselect"
+              value={this.props.selectedGenre}
+              onChange={this.props.onSelectedGenreChange}
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option">
+                Todos
+              </option>
+              <option value="action" data-testid="select-option">
+                Ação
+              </option>
+              <option value="comedy" data-testid="select-option">
+                Comédia
+              </option>
+              <option value="thriller" data-testid="select-option">
+                Suspense
+              </option>
+            </select>
+          </label>
         </form>
       </div>
     );
