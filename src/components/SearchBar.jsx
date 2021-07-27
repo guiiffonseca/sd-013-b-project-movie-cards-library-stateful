6595ff1ae1200b04b8069896dcbeaa574227bae0
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-07-27 15:06:37 -0300

    3 e 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,7 +7,22 @@ class SearchBar extends React.Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-
+        <label data-testid="text-input-label">
+          Inclui o texto:
+          <input data-testid="text-input" 
+          type="text" 
+          onChange={ onSearchTextChange }
+          value={ searchText }
+          />
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input data-testid="checkbox-input" 
+          type="checkbox" 
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          />
+        </label>
       </form>
     )
   }
