b8f1826524aa0506e23350901aaa81184e5c10ee
guilherme <guilhermedc93@gmail.com>
2021-07-27 15:53:13 -0300

    conclui passo 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+export default class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <span> teste </span>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: propTypes.func.isRequired,
+};
