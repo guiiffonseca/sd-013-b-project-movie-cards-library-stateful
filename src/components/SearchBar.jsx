287606abde482912ad20a5d0691e9ecb48e82235
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-07-27 15:07:43 -0300

    Realiza requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange
+    } = this.props;
+
+    return (
+      <nav>
+        <h1>oi</h1>
+      </nav>
+    );
+  }
+}
+
+export default SearchBar;
