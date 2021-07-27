92d41fe539ffa4a7a101b9ec76f43bba98057ceb
AC-Oliveira <allancardoso21@gmail.com>
2021-07-27 17:57:22 -0300

    Requisitos 1 ao 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import SearchBar from './SearchBar';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     const { movies } = this.props;
-
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <div data-testid="movie-list" className="movie-list">
+          { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        </div>
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,66 @@
 // implement SearchBar component here
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre,
+      onSelectedGenreChange } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="input-text">
+          Inclui o texto:
+          <input
+            type="text"
+            onChange={ onSearchTextChange }
+            id="input-text"
+            data-testid="text-input"
+            value={ searchText }
+            name="searchText"
+          />
+        </label>
+        <label
+          htmlFor="favoritos-checkbox"
+          data-testid="checkbox-input-label"
+        >
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="favoritos-checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            name="bookmarkedOnly"
+          />
+        </label>
+        <label htmlFor="genre-select" data-testid="select-input-label">
+          Filtrar por gênero:
+          <select
+            name="selectedGenre"
+            id="genre-select"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
