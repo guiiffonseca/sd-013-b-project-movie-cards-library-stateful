38fe6a9cebee16032856b21bbe788fe7b255ae57
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-07-27 16:05:57 -0300

    Requisito 14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,14 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Title from './Title';
 import Subtitle from './Subtitle';
 import ImagePath from './ImagePath';
 import Storyline from './Storyline';
 import Rating from './NewRating';
 import Genre from './Genre';
+import Button from './Button';
 
 export default class AddMovie extends React.Component {
   constructor() {
@@ -21,6 +24,7 @@ export default class AddMovie extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange({ target }) {
@@ -29,8 +33,21 @@ export default class AddMovie extends React.Component {
     });
   }
 
+  handleClick(reset) {
+    reset(this.state);
+    this.setState({
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
         <Title title={ title } onChange={ this.handleChange } />
@@ -39,7 +56,12 @@ export default class AddMovie extends React.Component {
         <Storyline storyline={ storyline } onChange={ this.handleChange } />
         <Rating rating={ rating } onChange={ this.handleChange } />
         <Genre genre={ genre } onChange={ this.handleChange } />
+        <Button onClick={ () => this.handleClick(onClick) } />
       </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
