044362544090a80a81fb7b9fde31146f9dc4a967
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-27 18:58:41 -0300

    Requisito 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.filterMovies = this.filterMovies.bind(this);
 
-    const p = this.props;
+    const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: this.props,
+      movies,
     };
   }
 
@@ -21,8 +23,18 @@ class MovieLibrary extends React.Component {
     const s = this.state;
     const { name, type } = e.target;
     let { value } = e.target;
-    if (type === 'checkbox')value = !s.bookmarkedOnly;
+    if (type === 'checkbox')value = e.target.checked;
     this.setState({ [name]: value });
+  }
+
+  filterMovies() {
+    const s = this.state;
+    const filtered = s.movies.filter((x) => (x.title.includes(s.searchText)
+    || x.subtitle.includes(s.searchText)
+    || x.storyline.includes(s.searchText))
+    && x.genre.includes(s.selectedGenre));
+    return s.bookmarkedOnly === false ? filtered
+      : filtered.filter((x) => x.bookmarked === true);
   }
 
   render() {
@@ -37,6 +49,7 @@ class MovieLibrary extends React.Component {
           selectedGenre={ s.selectedGenre }
           onSelectedGenreChange={ this.onInputChange }
         />
+        <MovieList movies={ this.filterMovies() } />
       </div>
     );
   }
