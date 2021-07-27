6733d5111ab3fbc3af72278e3914f040296c7257
Arthur-Jr <arthurjr1906@gmail.com>
2021-07-27 16:51:40 -0300

    Adiciona o sexto requisito.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,30 @@
-// implement AddMovie component here
+import React from 'react';
+import PropType from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <form action=""></form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropType.func.isRequired,
+}
+
+export default AddMovie;
