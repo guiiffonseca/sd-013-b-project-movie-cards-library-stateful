bf1eec43c806ece0e9f84af2e4cffdc072d65a44
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-07-27 18:49:56 -0300

    Requisito 8 funcionando

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   render() {
+    const { title } = this.props;
     return (
       <div>
         <form action="" data-testid="add-movie-form">
-          Formulario
+          <label htmlFor="a" data-testid="title-input-label">
+            Título
+            <input value={ title } type="text" name="" id="a" data-testid="title-input" />
+          </label>
         </form>
       </div>
     );
   }
 }
+
+AddMovie.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default AddMovie;
