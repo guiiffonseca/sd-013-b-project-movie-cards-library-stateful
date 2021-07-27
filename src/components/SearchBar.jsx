ad3b66ca1afecaaea86f8f366c1c7f0013178de7
alanmdf <alanmoreirascr@gmail.com>
2021-07-27 15:11:30 -0300

    Requisitos 6 e 7 implementados, falta testar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
     return (
       <form action="" data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
