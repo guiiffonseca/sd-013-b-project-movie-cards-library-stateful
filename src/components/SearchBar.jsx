9e9fa3ef04cf5c31b1c1c5701f3ab36d9f138a00
Carol_Inglethe <carolinainglethe@gmail.com>
2021-07-27 15:24:14 -0300

    2 - Renderize um formulário dentro de <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,34 @@
 // implement SearchBar component here
 import React from 'react';
+import propTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
-    return(
-      
-    )
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <h1> search bar</h1>
+        <h1>
+          { searchText }
+          { onSearchTextChange }
+          { bookmarkedOnly }
+          { onBookmarkedChange }
+          { selectedGenre }
+          { onSelectedGenreChange }
+        </h1>
+      </form>
+    );
   }
 }
 
 SearchBar.propTypes = {
-    searchText: propTypes.string,
-    onSearchTextChange: propTypes.func,
-    bookmarkedOnly: propTypes.bool,
-    onBookmarkedChange: propTypes.func,
-    selectedGenre: propTypes.string,
-    onSelectedGenreChange: propTypes.func,
-}
+  searchText: propTypes.string.isRequired,
+  onSearchTextChange: propTypes.func.isRequired,
+  bookmarkedOnly: propTypes.bool.isRequired,
+  onBookmarkedChange: propTypes.func.isRequired,
+  selectedGenre: propTypes.string.isRequired,
+  onSelectedGenreChange: propTypes.func.isRequired,
+};
 
 export default SearchBar;
