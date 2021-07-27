e2bf3edf922bf22c388f35cf69e783e0804e3b6e
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-07-27 18:13:08 -0300

    Adiciona requisitos 1 ao 5 sem erros no lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -15,15 +16,20 @@ class MovieLibrary extends Component {
   }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
 
 export default MovieLibrary;
