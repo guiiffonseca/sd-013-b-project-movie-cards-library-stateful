593732b8f9f67a1f4c4c6081011e4784c2771472
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-27 19:15:54 -0300

    Requisito 19 - addMovie function

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class MovieLibrary extends React.Component {
 
     this.onInputChange = this.onInputChange.bind(this);
     this.filterMovies = this.filterMovies.bind(this);
+    this.addMovie = this.addMovie.bind(this);
 
     const { movies } = this.props;
     this.state = {
@@ -36,6 +38,21 @@ class MovieLibrary extends React.Component {
       : filtered.filter((x) => x.bookmarked === true);
   }
 
+  addMovie(params) {
+    const newMovie = {
+      title: params.title,
+      subtitle: params.subtitle,
+      storyline: params.storyline,
+      rating: params.rating,
+      imagePath: params.imagePath,
+      bookmarked: params.bookmarked,
+      genre: params.genre,
+    };
+    this.setState((prevState) => ({
+      movies: [...prevState.movies, newMovie],
+    }));
+  }
+
   render() {
     const s = this.state;
     return (
@@ -49,13 +66,10 @@ class MovieLibrary extends React.Component {
           onSelectedGenreChange={ this.onInputChange }
         />
         <MovieList movies={ this.filterMovies() } />
+        <AddMovie onClick={ this.addMovie } />
       </div>
     );
   }
 }
-
-MovieLibrary.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
 
 export default MovieLibrary;
