f76a602726efdd80151be909230ec47504aa2e82
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-07-27 14:58:31 -0300

    Requisito 1 Finalizado

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+export default class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <SearchBar
+        searchText=""
+        onSearchTextChange=""
+        bookmarkedOnly=""
+        onBookmarkedChange=""
+        selectedGenre=""
+        onSelectedGenreChange=""
+      />
+    );
+  }
+}
