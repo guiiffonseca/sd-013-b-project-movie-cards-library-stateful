fc33c5614301368676013b2581a0f1ccaad6227b
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-27 16:46:12 -0300

    resolve ate o 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,33 +15,45 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleData = (event) => {
+    const { name } = event.target;
+    const value = event.target.type === 'checkbox'
+      ? event.target.checked
+      : event.target.value;
+
+    this.setState(({
+      [name]: value,
+    }));
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+
     return (
-      <form>
-        <label htmlFor="title">
+      <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
           Título
-          <input type="text" name="title" id="title" />
+          <input type="text" name="title" id="title" value={ title } data-testid="title-input" onChange={ this.handleData } />
         </label>
-        <label htmlFor="subtitle">
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
           Subtítulo
-          <input type="text" name="subtitle" id="subtittle" />
+          <input type="text" name="subtitle" id="subtittle" data-testid="subtitle-input" value={ subtitle } onChange={ this.handleData } />
         </label>
-        <label htmlFor="img">
+        <label htmlFor="imagePath" data-testid="image-input-label">
           Imagem
-          <input type="text" name="img" id="img" />
+          <input type="text" name="imagePath" id="imagePath" data-testid="image-input" value={ imagePath } onChange={ this.handleData } />
         </label>
-        <label htmlFor="storyline">
-          {' '}
+        <label htmlFor="storyline" data-testid="storyline-input-label">
           Sinopse
-          <input type="text" name="storyline" id="storyline" />
+          <textarea name="storyline" id="storyline" data-testid="storyline-input" value={ storyline } onChange={ this.handleData } cols="30" rows="10" />
         </label>
         <label htmlFor="rating">
           Avaliação
-          <input type="text" name="rating" id="rating" />
+          <input type="text" name="rating" id="rating" value={ rating } onChange={ this.handleData } />
         </label>
         <label htmlFor="genre">
           Gênero
-          <input type="text" name="genre" id="genre" />
+          <input type="text" name="genre" id="genre" value={ genre } onChange={ this.handleData } />
         </label>
       </form>
     );
