c807ec16d8c7633b676eaf0273cd6dca25bd1eeb
Guilherme <guilhermerg1234@gmail.com>
2021-07-27 15:30:44 -0300

    Iniciando Projeto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -13,7 +13,7 @@ class MovieLibrary extends Component {
   render() {
     return (
       <div>
-        <h2> My awesome movie library </h2>
+        <h2> My awesome movie library. </h2>
         <SearchBar />
         <MovieList movies={this.props.movies} />
         <AddMovie />
