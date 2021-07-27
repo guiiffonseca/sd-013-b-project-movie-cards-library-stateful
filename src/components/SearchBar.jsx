62fc521aecd3a402326e30d7bad043ef72cd388e
Polyana Balotin <polybalotin@hotmail.com>
2021-07-27 15:28:35 -0300

    requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,7 +13,14 @@ class SearchBar extends Component {
     } = this.props;
 
     return (
-      
-    )
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="text-input">
+          Inclui o texto:
+          <input data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
+        </label>
+      </form>
+    );
   }
 }
+
+export default SearchBar;
