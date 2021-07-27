2344696d9d5ffb30adc5f1a67e90ffa2c8db052e
Marcos Estevao <dusdoom@gmail.com>
2021-07-27 20:10:34 -0300

    added initial states and <SearchBar /> to <MovieLibrary />

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,26 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+export default class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [movies],
+    };
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <SearchBar
+        searchText={ searchText }
+        bookmarkedOnly={ bookmarkedOnly }
+        selectedGenre={ selectedGenre }
+      />
+    );
+  }
+}
