86a7a667a18c03848946518ffe1697ff69b85073
guilherme <guilhermedc93@gmail.com>
2021-07-27 15:30:38 -0300

    conclui etapa 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import movies from './data';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar searchText="olá" />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import propTypes from 'prop-types'
+
+export default class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
+    return (
+      <span>teste</span>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: propTypes.string.isRequired,
+  onSearchTextChange: propTypes.func.isRequired,
+  bookmarkedOnly: propTypes.bool.isRequired,
+  onBookmarkedChange: propTypes.func.isRequired,
+  selectedGenre: propTypes.string.isRequired,
+  onSelectedGenreChange: propTypes.func.isRequired,
+};
+
