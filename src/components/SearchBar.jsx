445a3c8d762b1c0488ebd28fa15326ebbb60ae2f
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-07-27 17:08:26 -0300

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 class SearchBar extends React.Component {
     render() {
         return(
-            <h1>searchbar</h1>
+            <form action="" data-testid="search-bar-form"></form>
         )
     }
     
