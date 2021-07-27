6db6c496692e95f241d878bab6192607e8477900
Marcos Estevao <dusdoom@gmail.com>
2021-07-27 15:21:33 -0300

    added app.js basic structure + modifying SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <h1>teste</h1>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
