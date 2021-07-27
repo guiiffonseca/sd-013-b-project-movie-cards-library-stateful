35d20669f29c9893ec5b8ef4c105c70136825293
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-27 20:03:49 -0300

    requisito 13 - concluido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Input from './Input';
+import Select from './Select';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -23,53 +24,59 @@ class AddMovie extends React.Component {
     });
   }
 
+  renderInput = ([id, labelText, value, type = 'text', name]) => (
+    <Input
+      id={ id }
+      labelText={ labelText }
+      name={ name }
+      onChange={ this.handleChange }
+      type={ type }
+      value={ value }
+    />
+  )
+
+  renderFormHandles = ({ genre, imagePath, rating, storyLine, subtitle, title }) => (
+    <>
+      {[
+        this.renderInput(['title', 'Título', title]),
+        this.renderInput(['subtitle', 'Subtítulo', subtitle]),
+        this.renderInput(['image', 'Imagem', imagePath, 'text', 'imagePath']),
+      ]}
+      <label data-testid="storyline-input-label" htmlFor="storyline">
+        Sinopse
+        <textarea
+          name="storyLine"
+          id="storyLine"
+          defaultValue={ storyLine }
+          data-testid="storyline-input"
+          onChange={ this.handleChange }
+        />
+      </label>
+      {
+        this.renderInput(['rating', 'Avaliação', rating, 'number'])
+      }
+      <Select
+        id="genre"
+        genres={ [
+          { value: 'action', optionText: 'Ação' },
+          { value: 'comedy', optionText: 'Comédia' },
+          { value: 'thriller', optionText: 'Suspense' },
+        ] }
+        labelText="Gênero"
+        onChange={ this.handleChange }
+        value={ genre }
+      />
+    </>
+  )
+
   render() {
-    const { genre, imagePath, rating, storyLine, subtitle, title } = this.state;
     const { onClick } = this.props;
 
-    console.log(genre, onClick);
+    console.log(onClick);
 
     return (
       <form data-testid="add-movie-form">
-        <Input
-          id="title"
-          labelText="Título"
-          onChange={ this.handleChange }
-          type="text"
-          value={ title }
-        />
-        <Input
-          id="subtitle"
-          labelText="Subtítulo"
-          onChange={ this.handleChange }
-          type="text"
-          value={ subtitle }
-        />
-        <Input
-          id="image"
-          labelText="Imagem"
-          name="imagePath"
-          onChange={ this.handleChange }
-          type="text"
-          value={ imagePath }
-        />
-        <label data-testid="storyline-input-label" htmlFor="storyline">
-          Sinopse
-          <textarea
-            name="storyLine"
-            id="storyLine"
-            defaultValue={ storyLine }
-            data-testid="storyline-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <Input
-          id="rating"
-          labelText="Avaliação"
-          onChange={ this.handleChange }
-          type="number"
-          value={ rating }
-        />
+        { this.renderFormHandles(this.state) }
       </form>
     );
   }
