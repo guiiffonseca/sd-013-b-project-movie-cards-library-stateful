0310c1a3c3c6e779727d8f32a4bab60d93857a8f
Alex <alexcorreas1994@gmail.com>
2021-07-27 17:15:26 -0300

    AddMovie Criado- requisito 6 e Form criado requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+export default class AddMovie extends Component {
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+};
