2529edda0fde1fa1d3eaeea57ccaa339f3c56577
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-07-27 16:00:12 -0300

    Realiza requisitos 2 ao 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,16 +10,43 @@ class SearchBar extends Component {
     } = this.props;
 
     return (
-      <nav>
-        <h1>
-          { searchText }
-          { onSearchTextChange }
-          { bookmarkedOnly }
-          { onBookmarkedChange }
-          { selectedGenre }
-          { onSelectedGenreChange }
-        </h1>
-      </nav>
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            id="text-input"
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label htmlFor="checkbox-input-label" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            id="checkbox-input-label"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="select-input"
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+
+      </form>
     );
   }
 }
