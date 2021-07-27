fe48810a28090f8619979dbebd2889dd7baba540
Guilherme Viana <guilhermehviana01@gmail.com>
2021-07-27 14:57:20 -0300

    Iniciando o mais sensacional dos projetos :statue_of_liberty: :green_heart: :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+export default class MovieLibrary extends Component {
+  constructor() {
+    super();
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> The amazing movie library </h2>
+        <MovieList />
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
