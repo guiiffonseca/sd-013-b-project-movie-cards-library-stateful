c311eec0e6b32de801d93de179d87be8b375ab31
iroDevs <ironegrolol@gamil.com>
2021-07-27 15:54:34 -0300

    do 1 ao 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  const call = () => {
+    const texto = 'teste';
+    console.log(texto);
+  };
+
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText=""
+        onSearchTextChange={ call }
+        bookmarkedOnly={ false }
+        onBookmarkedChange={ call }
+        selectedGenre=""
+        onSelectedGenreChange={ call }
+      />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,50 @@
 // implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText } = this.props;
+    const { onSearchTextChange } = this.props;
+    const { bookmarkedOnly } = this.props;
+    const { onBookmarkedChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="Incluiotexto" data-testid="text-input-label">
+
+          Inclui o texto:
+          <input
+            id="Incluiotexto"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+        
+        <label htmlFor="check" data-testid="checkbox-input-label">
+          Mostrar somente favoritos:
+          <input
+         data-testid="checkbox-input"
+            id="check"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+
+  onSearchTextChange: PropTypes.string.isRequired,
+  searchText: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+
+};
+
+export default SearchBar;
