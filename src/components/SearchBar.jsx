01abbeddd9faf869d5a4c6d3ab68a1f32a0d3ad5
Carol_Inglethe <carolinainglethe@gmail.com>
2021-07-27 16:05:14 -0300

    Requesitos 1 ao 5 concluido - Componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,6 +6,7 @@ class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly,
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
