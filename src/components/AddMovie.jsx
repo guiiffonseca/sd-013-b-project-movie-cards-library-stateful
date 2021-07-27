d3cdc0563130992a9bc94d9dad47077782ba7a9f
JoãoBDf <joaobdf@hotmail.com>
2021-07-27 16:44:19 -0300

    Requisito 6 Finalizado sem lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React, { Component } from "react";
+
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.setState;
+    return(
+      <div>
+        <h1>Opa</h1>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
