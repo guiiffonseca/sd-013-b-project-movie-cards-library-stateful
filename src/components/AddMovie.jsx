a1d74011f609b1d464f3dde6be668cf9d2df1ae4
Marcos Estevao <dusdoom@gmail.com>
2021-07-27 19:42:20 -0300

    added required forms components

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import DefaultTextInput from './DefaultTextInput';
+import DefaultTextAreaInput from './DefaultTextAreaInput';
+import DefaultNumberInput from './DefaultNumberInput';
+import DefaultSelectInput from './DefaultSelectInput';
+import DefaultButton from './DefaultButton';
 
 export default class AddMovies extends React.Component {
   constructor() {
@@ -8,11 +13,12 @@ export default class AddMovies extends React.Component {
       subtitle: '',
       title: '',
       imagePath: '',
-      storyLine: '',
+      storyline: '',
       rating: 0,
       genre: 'action',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(event) {
@@ -21,8 +27,21 @@ export default class AddMovies extends React.Component {
     });
   }
 
+  handleClick() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
-    const { title, subtitle, imagePath, rating, genre } = this.state;
+    const { title, subtitle, imagePath, rating, storyline, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <DefaultTextInput
@@ -46,28 +65,31 @@ export default class AddMovies extends React.Component {
           value={ imagePath }
           handleChange={ this.handleChange }
         />
-        <label htmlFor="storyline" data-testid="storyline-input-label">
-          Sinopse
-          <textarea
-            onChange={ this.handleChange }
-            data-testid="storyline-input"
-            name="storyline"
-            id=""
-            cols="30"
-            rows="10"
-          />
-        </label>
-        <label htmlFor="rating" data-testid="rating-input-label">
-          Avaliação
-          <input
-            value={ rating }
-            data-testid="rating-input"
-            type="number"
-            name="rating"
-            onChange={ this.handleChange }
-          />
-        </label>
+        <DefaultTextAreaInput
+          value={ storyline }
+          name="storyline"
+          testName="storyline"
+          labelText="Sinopse"
+          handleChange={ this.handleChange }
+        />
+        <DefaultNumberInput
+          value={ rating }
+          name="rating"
+          labelText="Avaliação"
+          handleChange={ this.handleChange }
+        />
+        <DefaultSelectInput
+          handleChange={ this.handleChange }
+          name="genre"
+          labelText="Gênero"
+          value={ genre }
+        />
+        <DefaultButton labelText="Adicionar filme" onClick={ this.handleClick } />
       </form>
     );
   }
 }
+
+AddMovies.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
