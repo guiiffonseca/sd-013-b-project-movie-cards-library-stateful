a06039da976860d9b652ecf2d128237e6fa2f448
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-27 15:28:35 -0300

    adiciona o exercicio anterior

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,16 +4,14 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = movie;
+    const { props } = this;
+    const { title, subtitle, storyline, imagePath, rating } = props.movie;
     return (
-      <div className="movie-card" data-testid="movie-card">
-        <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
-        <div className="movie-card-body">
-          <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
-        </div>
+      <div className="movie-card movie-card-body">
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <img src={ imagePath } alt="" className="movie-card-image" />
         <Rating rating={ rating } />
       </div>
     );
@@ -25,8 +23,9 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
-    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
+
   }).isRequired,
 };
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    const { rating } = this.props;
+    const { props } = this;
+    const { rating } = props;
     return (
-      <div className="movie-card-rating" data-testid="rating">
-        <span className="rating">{rating}</span>
+      <div className="rating">
+        {rating}
+        {console.log(rating)}
       </div>
     );
   }
 }
 
-Rating.propTypes = { rating: PropTypes.number };
-
-Rating.defaultProps = {
-  rating: 'undefined',
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
