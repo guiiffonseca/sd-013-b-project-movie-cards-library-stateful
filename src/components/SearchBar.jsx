12e6c4e4e1f914f68f9b4da960f05128447d2003
EddieRAM <edusacarneiro@gmail.com>
2021-07-27 15:41:09 -0300

    Requisite 1 done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,44 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    const { search } = this.state;
+    return (
+      <section className="search-bar">
+        <input
+          type="text"
+          name="search"
+          value={ search }
+          id="search"
+          onChange={ this.handleChange }
+        />
+      </section>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  movie: PropTypes.exact({
+    searchText: PropTypes.string,
+    bookmarkedOnly: PropTypes.bool,
+    selectedGenre: PropTypes.string,
+  }).isRequired,
+};
+
+export default SearchBar;
