69c1d2b90ec4818b6671a46e333949c0bd7048df
Alex <alexcorreas1994@gmail.com>
2021-07-27 15:39:36 -0300

    MovieLibrary Comentado para requisito 1 passar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,36 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import MovieList from './MovieList';
-import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import MovieList from './MovieList';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
+// class MovieLibrary extends Component {
+//   constructor(props) {
+//     super(props);
 
-  render() {
-    return (
-      <div>
-        <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={ this.props.movies } />
-        <AddMovie />
-      </div>
-    );
-  }
-}
+//     this.state = {
+//       searchText: '',
+//     };
 
-export default MovieLibrary;
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+
+//   handleChange(event) {
+//     console.log(event.target.value);
+//     this.setState({ searchText: event.target.value });
+//   }
+
+//   render() {
+//     const { searchText } = this.state;
+//     return (
+//       <div>
+//         <h2> My awesome movie library </h2>
+//         <SearchBar searchText={ this.handleChange } />
+//         <MovieList movies={ this.state.movies } />
+//         <AddMovie />
+//       </div>
+//     );
+//   }
+// }
+
+// export default MovieLibrary;
