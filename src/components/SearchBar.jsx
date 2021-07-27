77067421d77654e46e20c74bb754d22868741bec
Guilherme <guilhermerg1234@gmail.com>
2021-07-27 16:46:37 -0300

    Ex1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,24 @@
 // implement SearchBar component here
+import React from 'react';
+
+const SearchBar = (props) => {
+        let searchText = props.searchText;
+        let onSearchTextChange = props.onSearchTextChange;
+        let bookmarkedOnly = props.bookmarkedOnly;
+        let onBookmarkedChange = props.onBookmarkedChange;
+        let selectedGenre = props.selectedGenre;
+        let onSelectedGenreChange = props.onSelectedGenreChange;
+
+        return (
+            <div>
+                {searchText}
+                {onSearchTextChange}
+                {bookmarkedOnly}
+                {onBookmarkedChange}
+                {selectedGenre}
+                {onSelectedGenreChange}
+            </div>
+        )
+    }
+
+export default SearchBar;
