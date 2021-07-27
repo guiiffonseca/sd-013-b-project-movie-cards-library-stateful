67d5d56283afed473dc9458d1253196f2b5f207f
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-07-27 14:13:03 -0300

    First Commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <label htmlFor="">
+        <input type="text" />
+      </label>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+};
