7033f3a4c1b28eae57607afce372c50442640dbd
GenesisHenriques <genesishenriques7@gmail.com>
2021-07-27 18:28:45 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,12 +12,26 @@ export default class AddMovie extends React.Component {
       genre: 'action',
     };
   }
-  
+
+  inputText = ({ target }) => {
+    this.setState({ title: target.value });
+  }
+
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     const { onclick } = this.props;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.inputText }
+          />
+        </label>
+      </form>
     );
   }
 }
