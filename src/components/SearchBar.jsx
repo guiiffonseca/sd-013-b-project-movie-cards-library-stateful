ef3bfe07a8cd2db127e23939dfbe4e75f92aa091
LucianoJunior <lucianojunior4242@gmail.com>
2021-07-27 16:15:17 -0300

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,9 @@ export default class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly, 
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
-    return <h1>test</h1>;
+    return (<form data-testid="search-bar-form">
+      
+    </form>);
   }
 }
 
