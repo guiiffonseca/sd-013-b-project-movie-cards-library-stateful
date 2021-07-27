1d9fa4cb7bc29196053e3783f0bdd47050eb1676
Marcos Estevao <dusdoom@gmail.com>
2021-07-27 19:36:35 -0300

    linked default value of DefaultTextAreaInput

--- src/components/DefaultTextAreaInput.jsx
+++ src/components/DefaultTextAreaInput.jsx
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 
 export default class DefaultTextAreaInput extends React.Component {
   render() {
-    const { name, testName, labelText, handleChange } = this.props;
+    const { name, value, testName, labelText, handleChange } = this.props;
     return (
       <label htmlFor={ name } data-testid={ `${testName}-input-label` }>
         {labelText}
         <textarea
+          value={ value }
           onChange={ handleChange }
           data-testid={ `${name}-input` }
           name={ name }
@@ -24,4 +25,5 @@ DefaultTextAreaInput.propTypes = {
   testName: PropTypes.string.isRequired,
   labelText: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
 };
