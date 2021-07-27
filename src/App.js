601118b7c1c608041f20c11ba3c92f6f71ee79f2
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-07-27 17:07:22 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
+
+const propsSearchBar = {
+  searchText: '',
+  onSearchTextChange: () => {},
+  bookmarkedOnly: false,
+  onBookmarkedChange: () => {},
+  selectedGenre: '',
+  onSelectedGenreChange: () => {},
+};
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-// implement SearchBar component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+
+class SearchBar extends React.Component {
+    render() {
+        return(
+            <h1>searchbar</h1>
+        )
+    }
+    
+}
+
+
+export default SearchBar;
