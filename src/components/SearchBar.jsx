077e8d2502b4e6d494d6d9d6d6de4d9bf6a2caac
Henrique Cursino <henriquecursino46@gmail.com>
2021-07-27 18:00:51 -0300

    Requisito 5 e lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -27,12 +27,26 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
+
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
 }
 
-SearchBar.ProTypes = {
+SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
