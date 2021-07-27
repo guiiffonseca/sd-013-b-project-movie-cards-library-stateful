7d996a076ef8cd61198ab7c4bd886c65dbf39575
Carol_Inglethe <carolinainglethe@gmail.com>
2021-07-27 15:07:58 -0300

    Criando componente SearchBar e validando suas props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return(
+      
+    )
+  }
+}
+
+SearchBar.propTypes = {
+    searchText: propTypes.string,
+    onSearchTextChange: propTypes.func,
+    bookmarkedOnly: propTypes.bool,
+    onBookmarkedChange: propTypes.func,
+    selectedGenre: propTypes.string,
+    onSelectedGenreChange: propTypes.func,
+}
+
+export default SearchBar;
