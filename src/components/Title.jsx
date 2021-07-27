bed745f8df40d5c6e5318b72b581551a95ad88f9
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-07-27 15:23:39 -0300

    Requisito 8, 9 e 10

---
+++ src/components/Title.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Title extends Component {
+  render() {
+    const { title, onChange } = this.props;
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
+        Título
+        <input
+          type="text"
+          name="title"
+          value={ title }
+          onChange={ onChange }
+          data-testid="title-input"
+        />
+      </label>
+    );
+  }
+}
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
