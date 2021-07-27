91b97804056b35df3436603829026e1adeaed04d
Alex <alexcorreas1994@gmail.com>
2021-07-27 15:37:25 -0300

    requisito 1 feito

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <form id="search-bar-form">
+        <label htmlFor="searchText">
+          Busca de Filmes
+          <input type="search" name="searchText" id="searchText" />
+          <button type="submit">
+            Procurar
+          </button>
+        </label>
+      </form>
+    );
+  }
+}
