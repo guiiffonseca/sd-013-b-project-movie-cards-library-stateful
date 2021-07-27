1d1c593ed347ec5496a6b32bcf9c28fa8f101c4c
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-27 15:58:08 -0300

    requisito 3 - concluido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,7 +9,19 @@ const SearchBar = ({
   onSelectedGenreChange,
 }) => (
   <form data-testid="search-bar-form" >
-
+    <label 
+      data-testid="text-input-label"
+      htmlFor="search"
+    >
+      Inclui o texto:
+      <input
+        data-testid="text-input"
+        id="search"
+        onChange={ onSearchTextChange }
+        type="text"
+        value={ searchText }
+      />
+    </label>
   </form>
 )
 
