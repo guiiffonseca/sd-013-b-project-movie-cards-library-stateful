a2c1938bab526a81e739705c3569214d14e2b41f
marcus <marcus.rodrigues10@hotmail.com>
2021-07-27 15:06:48 -0300

    exercise 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, selectedGenre, onSelectedGenrechage } = this.props;
+
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenrechage,
+    } = this.props;
+
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="searchText" data-testid="text-input-label">
@@ -13,6 +21,19 @@ export default class SearchBar extends Component {
             data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
+          />
+        </label>
+        <label
+          htmlFor="favorites"
+          data-testid="checkbox-input-label"
+        >
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="favorites"
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
           />
         </label>
       </form>
