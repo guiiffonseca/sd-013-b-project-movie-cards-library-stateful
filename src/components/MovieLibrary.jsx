90cfef93004f8daf25f1f86ceff53d219e378aec
Miguel <migmenezes.mm2@gmail.com>
2021-07-27 20:22:24 -0300

    lint aos poucos

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-
-import MovieList from './MovieList';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={ this.props.movies } />
         <AddMovie />
       </div>
     );
   }
 }
-
+PropTypes.MovieLibrary = {
+  movies: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.bool,
+  ]),
+};
 export default MovieLibrary;
