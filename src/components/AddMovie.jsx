6895bb7da63df7f650fc87575ac8481809d584ef
Alex <alexcorreas1994@gmail.com>
2021-07-27 18:32:24 -0300

    Requisito 8- Input Título, Requisito 9-Input Subtítulo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export default class AddMovie extends Component {
   render() {
+    const { onChange, title, subtitle } = this.props;
     return (
       <form data-testid="add-movie-form">
-        <input type="text" />
+        <label htmlFor="title-input-label" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title-input"
+            data-testid="title-input"
+            value={ title }
+            onChange={ onChange }
+          />
+        </label>
+        <label htmlFor="subtitle-input-label" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle-input"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={ onChange }
+          />
+        </label>
       </form>
     );
   }
 }
 
 AddMovie.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
 };
