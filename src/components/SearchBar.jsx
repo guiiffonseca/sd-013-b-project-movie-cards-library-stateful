183d3fbe3b59074bad2ddbccb307f6517bf4aacb
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-07-27 15:22:58 -0300

    Soluciona requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -33,6 +33,25 @@ export default class SearchBar extends React.Component {
           />
 
         </label>
+
+        <label data-testid="select-input-label" htmlFor="selectInput">
+
+          Filtrar por gênero
+
+          <select
+            data-testid="select-input"
+            id="selectInput"
+            name="selectInput"
+            onChange={ onSelectedGenreChange }
+            value={ selectedGenre }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+
+        </label>
       </div>
     );
   }
