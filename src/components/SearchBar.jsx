9fe2e5563a4c0220b625b3fdb149ff8f98d7f6cc
Tenari <guilherme.tenarie@gmail.com>
2021-07-27 17:17:19 -0300

    Adiciona requisito 5 - Select with 4 options

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,21 +18,35 @@ class SearchBar extends React.Component {
             value={ searchText }
             onChange={ onSearchTextChange }
           />
-          <label
-            htmlFor="checkbox-input"
-            data-testid="checkbox-input-label"
-          >
-            Mostrar somente favoritos
-            <input
-              type="checkbox"
-              data-testid="checkbox-input"
-              checked={ bookmarkedOnly }
-              onChange={ onBookmarkedChange }
-            />
-
-          </label>
         </label>
+        <label
+          htmlFor="checkbox-input"
+          data-testid="checkbox-input-label"
+        >
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            name=""
+            data-testid="select-input"
+            id=""
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value=""> Todos </option>
+            <option data-testid="select-option" value="action"> Ação </option>
+            <option data-testid="select-option" value="comedy"> Comédia </option>
+            <option data-testid="select-option" value="thriller"> Suspense </option>
 
+          </select>
+        </label>
       </form>
     );
   }
