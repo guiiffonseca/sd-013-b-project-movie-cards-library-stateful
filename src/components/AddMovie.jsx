7483e19078c6c60b14733bae5f021edad3ec172a
Alef Sloan <Alef_sloan@hotmail.com>
2021-07-27 20:02:21 -0300

    Requisito 9 finalizado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,27 +5,40 @@ class AddMovie extends React.Component {
     super();
     this.state = {
       title: '',
+      subtitle: '',
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = (event) => {
-    this.setState({ title: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <div>
         <form action="" data-testid="add-movie-form">
           <label htmlFor="title-input-movie">
             Título
             <input
+              name="title"
               id="title-input-movie"
               type="text"
               value={ title }
               onChange={ this.handleChange }
               data-testid="title-input-label"
+            />
+          </label>
+          <label htmlFor="subtitle-input-movie" data-testid="subtitle-input-label">
+            Subtítulo
+            <input
+              name="subtitle"
+              id="subtitle-input-movie"
+              type="text"
+              value={ subtitle }
+              onChange={ this.handleChange }
+              data-testid="subtitle-input"
             />
           </label>
         </form>
