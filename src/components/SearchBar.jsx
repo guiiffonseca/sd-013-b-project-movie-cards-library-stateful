cbcec649ecc09466e3546659fbc0fbc00e3bc613
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-27 16:54:06 -0300

    requisito 5 - concluido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,49 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SearchBar = ({
-  searchText,
-  onSearchTextChange,
-  bookmarkedOnly,
-  onBookmarkedChange,
-  selectedGenre,
-  onSelectedGenreChange,
-}) => (
-  <form data-testid="search-bar-form">
-    {
-      console.log({
-        selectedGenre,
-        onSelectedGenreChange,
-      })
-    }
-    <label
-      data-testid="text-input-label"
-      htmlFor="search"
-    >
-      Inclui o texto:
-      <input
-        data-testid="text-input"
-        id="search"
-        onChange={ onSearchTextChange }
-        type="text"
-        value={ searchText }
-      />
-    </label>
-    <label
-      data-testid="checkbox-input-label"
-      htmlFor="bookmark"
-    >
-      Mostrar somente favoritos
-      <input
-        data-testid="checkbox-input"
-        id="bookmark"
-        onChange={ onBookmarkedChange }
-        type="checkbox"
-        checked={ bookmarkedOnly }
-      />
-    </label>
-  </form>
-);
+import Select from './Select';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label
+          data-testid="text-input-label"
+          htmlFor="search"
+        >
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            id="search"
+            onChange={ onSearchTextChange }
+            type="text"
+            value={ searchText }
+          />
+        </label>
+        <label
+          data-testid="checkbox-input-label"
+          htmlFor="bookmark"
+        >
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            id="bookmark"
+            onChange={ onBookmarkedChange }
+            type="checkbox"
+            checked={ bookmarkedOnly }
+          />
+        </label>
+        <Select
+          onSelectedGenreChange={ onSelectedGenreChange }
+          selectedGenre={ selectedGenre }
+        />
+      </form>
+    );
+  }
+}
 
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
