5bdfe878f7a5299be43d37d1dae712e60542a367
Polyana Balotin <polybalotin@hotmail.com>
2021-07-27 15:20:28 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import movies from './data';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
       <MovieList movies={ movies } />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onselectedGenreChange,
+    } = this.props;
+
+    return (
+      
+    )
+  }
+}
