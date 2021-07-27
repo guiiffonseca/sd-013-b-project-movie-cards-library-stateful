bd558cab060fb9316516bc30b077cc92f152718a
unknown <sergio.espp@gmail.com>
2021-07-27 19:48:07 -0300

    Requisitos 6 ao 11 e 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,2 +1,47 @@
-// implement AddMovie component here
-// Começando Projeto
+import React from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
+import Subtitle from './Subtitle';
+import Image from './Image';
+import Synopsis from './Synopsis';
+import MovieRating from './MovieRating';
+import Genre from './Genre';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <Title value={ title } onChange={ this.handleChange } />
+        <Subtitle value={ subtitle } onChange={ this.handleChange } />
+        <Image value={ imagePath } onChange={ this.handleChange } />
+        <Synopsis value={ storyline } onChange={ this.handleChange } />
+        <MovieRating value={ rating } onChange={ this.handleChange } />
+        <Genre value={ genre } onChange={ this.handleChange } />
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
