48a0bc226e4c51f9ea2549a35890fcc3adf1b5e0
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-27 18:47:25 -0300

    refatora para agradar o linter

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,10 @@
 // implement AddMovie component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import InputLabeled from './InputLabeled';
+import InputLabeledImage from './InputLabeledImage';
+import TextAreaLabeled from './TextAreaLabeled';
+import InputLabeledRating from './InputLabeledRating';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -31,29 +35,30 @@ class AddMovie extends React.Component {
 
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title" data-testid="title-input-label">
-          Título
-          <input type="text" name="title" id="title" value={ title } data-testid="title-input" onChange={ this.handleData } />
-        </label>
-        <label htmlFor="subtitle" data-testid="subtitle-input-label">
-          Subtítulo
-          <input type="text" name="subtitle" id="subtittle" data-testid="subtitle-input" value={ subtitle } onChange={ this.handleData } />
-        </label>
-        <label htmlFor="imagePath" data-testid="image-input-label">
-          Imagem
-          <input type="text" name="imagePath" id="imagePath" data-testid="image-input" value={ imagePath } onChange={ this.handleData } />
-        </label>
-        <label htmlFor="storyline" data-testid="storyline-input-label">
-          Sinopse
-          <textarea name="storyline" id="storyline" data-testid="storyline-input" value={ storyline } onChange={ this.handleData } cols="30" rows="10" />
-        </label>
-        <label htmlFor="rating" data-testid="rating-input-label">
-          Avaliação
-          <input type="number" name="rating" id="rating" data-testid="rating-input" value={ rating } onChange={ this.handleData } />
-        </label>
+        <InputLabeled
+          name="title"
+          value={ title }
+          handleData={ this.handleData }
+          text="Título"
+        />
+        <InputLabeled
+          name="subtitle"
+          value={ subtitle }
+          handleData={ this.handleData }
+          text="Subtítulo"
+        />
+        <InputLabeledImage handleData={ this.handleData } value={ imagePath } />
+        <TextAreaLabeled value={ storyline } handleData={ this.handleData } />
+        <InputLabeledRating value={ rating } handleData={ this.handleData } />
         <label htmlFor="genre" data-testid="genre-input-label">
           Gênero
-          <select name="genre" id="genre" data-testid="genre-input" value={ genre } onChange={ this.handleData }>
+          <select
+            name="genre"
+            id="genre"
+            data-testid="genre-input"
+            value={ genre }
+            onChange={ this.handleData }
+          >
             <option value="action" data-testid="genre-option"> Ação </option>
             <option value="comedy" data-testid="genre-option"> Comédia </option>
             <option value="thriller" data-testid="genre-option"> Suspense </option>
---
+++ src/components/TextAreaLabeled.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TextAreaLabeled extends React.Component {
+  render() {
+    const { value, handleData } = this.props;
+    return (
+      <label htmlFor="storyline" data-testid="storyline-input-label">
+        Sinopse
+        <input
+          type="text"
+          name="storyline"
+          id="storyline"
+          value={ value }
+          data-testid="storyline-input"
+          onChange={ handleData }
+        />
+      </label>
+    );
+  }
+}
+
+TextAreaLabeled.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleData: PropTypes.func.isRequired,
+};
+
+export default TextAreaLabeled;
