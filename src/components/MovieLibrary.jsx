9994ee675add83ebf187b965d063be65dee9bd54
Leonardo Martins <leonardomartins07@gmail.com>
2021-07-27 20:08:45 -0300

    requisito 1 ao 5

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,27 @@
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import movies from '../data';
+
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} movies={ movies }/>
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
 
