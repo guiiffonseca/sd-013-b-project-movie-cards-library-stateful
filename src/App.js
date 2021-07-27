63b3c86faa93aa4a3a63ff60e5f88e9f260299d8
enioandrade92 <enio.goulart@gmail.com>
2021-07-27 19:49:36 -0300

    Requisitos do SearchBar passaram

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
+
+import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <MovieLibrary /> */}
     </div>
   );
 }
