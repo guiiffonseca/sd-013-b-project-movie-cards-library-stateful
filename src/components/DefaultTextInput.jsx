bac3d168cd39a308be65d8a752a722851afe3908
Marcos Estevao <dusdoom@gmail.com>
2021-07-27 17:59:18 -0300

    added testName prop to <DefaultTextInput />

--- src/components/DefaultTextInput.jsx
+++ src/components/DefaultTextInput.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 export default class DefaultTextInput extends React.Component {
   render() {
-    const { name, value, handleChange, labelText } = this.props;
+    const { name, value, handleChange, labelText, testName} = this.props;
     return (
-      <label htmlFor={ name } data-testid={ `${name}-input-label` }>
+      <label htmlFor={ name } data-testid={ `${testName}-input-label` }>
         {labelText}
         <input
-          data-testid={ `${name}-input` }
+          data-testid={ `${testName}-input` }
           value={ value }
           type="text"
           name={ name }
@@ -20,6 +20,7 @@ export default class DefaultTextInput extends React.Component {
 }
 
 DefaultTextInput.propTypes = {
+  testName: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
