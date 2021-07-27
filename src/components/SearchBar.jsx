b32f479a799bfddf6f834437e008eb6fbd0d5115
Henrique Cursino <henriquecursino46@gmail.com>
2021-07-27 17:45:47 -0300

    Requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,6 +17,16 @@ class SearchBar extends React.Component {
             data-testid="text-input"
           />
         </label>
+
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
