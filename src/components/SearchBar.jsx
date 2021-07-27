fd5e2b7e5f61301b2d091edfd7b72d0ee73e5a39
Carol_Inglethe <carolinainglethe@gmail.com>
2021-07-27 16:03:07 -0300

    5 - Renderize um select dentro do formulário em <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,11 +31,24 @@ class SearchBar extends React.Component {
           <input
             type="checkbox"
             name="favoritos"
-            id="favoritos"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
           />
+        </label>
+        <label htmlFor="genero" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            name="genero"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
