3555036df400e8c31afc82b5e8a58d780264bdb1
JoãoBDf <joaobdf@hotmail.com>
2021-07-27 16:11:38 -0300

    Requisitos 4 e 5 finalizados sem lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,21 +7,41 @@ class SearchBar extends Component {
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
-      selectGenre,
+      selectedGenre,
       onSelectedGenreChange,
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label
-        htmlFor="search" 
-        data-testid="text-input-label"
-        >Inclui o texto</label>
-        <input
-        data-testid="text-input"
-        id="search"
-        value={ searchText }
-        onChange={ onSearchTextChange }
-        />
+        <label htmlFor="search" data-testid="text-input-label">
+          Inclui o texto
+          <input
+          type="text"
+            data-testid="text-input"
+            id="search"
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
+        </label>
+        <label htmlFor="checkBox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            data-testid="checkbox-input"
+            id="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <label htmlFor="select" data-testid="select-input-label">
+          Filtrar por gênero
+          <select value={ selectedGenre } id="select" 
+          data-testid="select-input" onChange={ onSelectedGenreChange }>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
