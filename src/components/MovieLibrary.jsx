3c178a26363b8a6739d76e4c05b9da4f6975db2c
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-07-27 16:07:55 -0300

    requisitos 1 ao 5

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,17 +5,28 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.props = {
+  //     //
+  //   };
+  // }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <SearchBar
+          searchText={searchText}
+          onSearchTextChange={onSearchTextChange}
+          bookmarkedOnly={bookmarkedOnly}
+          onBookmarkedChange={onBookmarkedChange}
+          selectedGenre={selectedGenre}
+          onSelectedGenreChange={onSelectedGenreChange}
+        />
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */}
       </div>
     );
   }
