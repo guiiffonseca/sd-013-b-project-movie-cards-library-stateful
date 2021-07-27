e2d14c9362b0ea9008a5cf879be116e0a07ced3a
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-07-27 17:53:15 -0300

    conclui requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,39 +1,87 @@
 import React from 'react';
+// https://stackoverflow.com/questions/45692537/proptypes-is-not-defined
+// Para resolver PropTypes is nor defined:
+// npm install --save prop-types
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-  constructor() {
-    super();
+  // constructor() {
+  //   super();
 
-    this.state = {
-      searchText: '',
-    };
-  }
+  //   this.state = {
+  //     searchText: '',
+  //   };
+
+  //   this.onSearchTextChange = this.onSearchTextChange.bind(this);
+  // }
+
+  // onSearchTextChange(event) {
+  //   this.setState({
+  //     searchText: event.target.value,
+  //   });
+  //   console.log(this.state.searchText);
+  // }
 
   render() {
-    const { searchText } = this.state;
-    // const onSearchTextChange = '';
-    // const bookmarkedOnly = '';
-    // const onBookmarkedChange = '';
-    // const selectedGenre = '';
-    // const onSelectedGenreChange = '';
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
 
     return (
-      <form data-testid="search-bar-form">
+      <form
+        data-testid="search-bar-form"
+      >
         <label
           htmlFor="text-input"
           data-testid="text-input-label"
         >
-          Inclui o texto
+          Inclui o texto=
           <input
+            onChange={ onSearchTextChange }
             id="text-input"
             data-testid="text-input"
-            value={ searchText }
             type="text"
+            value={ searchText }
+            esLint={
+              bookmarkedOnly
+              + onBookmarkedChange
+              + selectedGenre
+              + onSelectedGenreChange
+            }
           />
         </label>
+        <label
+          htmlFor="checkbob-input"
+        >
+          <input
+            type="checkbox"
+            id="checkbox-input"
+          />
+        </label>
+        <checkbox />
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string
+    .isRequired,
+  onSearchTextChange: PropTypes.func
+    .isRequired,
+  bookmarkedOnly: PropTypes.bool
+    .isRequired,
+  onBookmarkedChange: PropTypes.func
+    .isRequired,
+  selectedGenre: PropTypes.string
+    .isRequired,
+  onSelectedGenreChange: PropTypes.func
+    .isRequired,
+};
 
 export default SearchBar;
