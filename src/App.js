faef6a6209ca5764b913d9c9f82729b8f2568a9f
eaeniko <eaenikolai@gmail.com>
2021-07-27 17:12:12 -0300

    [Nikolai]  Req: 1-2-3 lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
 import './App.css';
-
 
 function App() {
   return (
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,33 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default class SearchBar extends React.Component {
-  constructor(props) {
+class SearchBar extends React.Component {
+  constructor() {
     super();
     this.state = {
-      searchText: ''
-    }
+      searchText: '',
+    };
     this.handleChange = this.handleChange.bind(this);
   }
-  
+
   handleChange(event) {
-    this.setState({searchText: event.target.value});
+    this.setState({ searchText: event.target.value });
   }
 
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
+    // const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange } = this.props;
 
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="search" data-testid="text-input-label">Inclui o texto:
-          <input 
-            onChange={this.handleChange}
-            type="text" 
-            id="search" 
-            value={searchText}
-            onChange={onSearchTextChange}
+        <label htmlFor="search" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            onChange={ this.handleChange }
+            type="text"
+            id="search"
+            value={ searchText }
+            onChange={ onSearchTextChange }
             data-testid="text-input"
           />
         </label>
@@ -34,12 +37,18 @@ export default class SearchBar extends React.Component {
             <option value="">2</option>
             <option value="">3</option>
             <option value="">4</option>
-          </select> 
+          </select>
 
         </label> */}
       </form>
 
     );
   }
-
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.string
+}
+
+export default  SearchBar;
