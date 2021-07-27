992d52e3f7b78f4c98b6b650916447da0e0a9c7c
GenesisHenriques <genesishenriques7@gmail.com>
2021-07-27 16:30:39 -0300

    Requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -20,6 +20,16 @@ export default class SearchBar extends React.Component {
             data-testid="text-input"
           />
         </label>
+
+        <label html="checkbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input 
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
