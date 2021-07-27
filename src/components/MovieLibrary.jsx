dacfc7e6ba7d42d0dfa9ee391ab068d132c99939
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-07-27 16:15:45 -0300

    lint :(

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.props = {
-  //     //
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.props = {
+      //
+    };
+  }
 
   render() {
     const { movies } = this.props;
@@ -18,18 +19,22 @@ class MovieLibrary extends Component {
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar
-          searchText={searchText}
-          onSearchTextChange={onSearchTextChange}
-          bookmarkedOnly={bookmarkedOnly}
-          onBookmarkedChange={onBookmarkedChange}
-          selectedGenre={selectedGenre}
-          onSelectedGenreChange={onSelectedGenreChange}
+          searchText={ searchText }
+          onSearchTextChange={ onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ onSelectedGenreChange }
         />
         <MovieList movies={ movies } />
-        {/* <AddMovie /> */}
+        <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
