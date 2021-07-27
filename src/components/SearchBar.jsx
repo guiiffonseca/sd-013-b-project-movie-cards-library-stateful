ded913a843ea928556590efb956f8118d7bc4322
JoãoBDf <joaobdf@hotmail.com>
2021-07-27 15:01:53 -0300

    Requisito 1 finalizado sem Lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { 
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default SearchBar;
