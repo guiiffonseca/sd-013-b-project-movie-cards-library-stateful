912d5ea486aab23e1549272f4813adf65fbdad19
LucianoJunior <lucianojunior4242@gmail.com>
2021-07-27 16:42:08 -0300

    Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ export default class SearchBar extends React.Component {
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
-          Inclui o texto
+          Inclui o texto:
           <input
             type="text"
             value={ searchText }
@@ -18,13 +18,26 @@ export default class SearchBar extends React.Component {
           />
         </label>
         <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
-          Mostrar somente favoritos
+          Mostrar somente favoritos:
           <input
             type="checkbox"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
           />
+        </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero:
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
