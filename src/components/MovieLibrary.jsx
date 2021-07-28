2d87e78b620f20043906db82517357804616604a
Marcos Estevao <dusdoom@gmail.com>
2021-07-27 21:12:42 -0300

    added unfinished MovieLibrary structure

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,27 +3,52 @@ import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 
 export default class MovieLibrary extends React.Component {
-  constructor() {
+  constructor(movies) {
     super();
     // const { movies } = this.props;
+    this.handleBookmarkedChange = this.handleBookmarkedChange.bind(this);
+    this.handleSelectedChange = this.handleSelectedChange.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      // movies: [movies],
+      movies,
     };
+  }
+
+  handleSearchChange(event) {
+    this.setState({
+      searchText: [event.target.value],
+    });
+  }
+
+  handleBookmarkedChange(event) {
+    this.setState({
+      bookmarkedOnly: [event.target.value],
+    });
+  }
+
+  handleSelectedChange(event) {
+    this.setState({
+      selectedGenre: [event.target.value],
+    });
   }
 
   render() {
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { movies } = this.state;
     return (
       <div>
         <SearchBar
           searchText={ searchText }
+          onSearchTextChange={ this.handleSearchChange }
           bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleBookmarkedChange }
           selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleSelectedChange }
         />
-        <MovieList />
+        <MovieList movies={ movies.movies } />
       </div>
     );
   }
