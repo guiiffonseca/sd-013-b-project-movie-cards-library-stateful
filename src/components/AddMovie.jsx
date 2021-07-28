cd7f9902eec5d08d8bdc7ca96af40063a7714094
Jessica Naiara <jessicanaiarag@gmail.com>
2021-07-27 21:22:44 -0300

    resolvendo requisitos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,47 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input type="text" name="title" value={ title } id="title" data-testid="title-input" onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" name="subtitle" value={ subtitle } id="subtitle" data-testid="subtitle-input" onChange={ this.handleChange } />
+        </label>
+      </form>
+    );
+  }
+}
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+export default AddMovie;
