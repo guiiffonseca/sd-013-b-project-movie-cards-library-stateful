b97142de2b618792453681995f88583dd4dfbd49
eaeniko <eaenikolai@gmail.com>
2021-07-27 22:06:23 -0300

    [Nikolai]  Req: 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -23,7 +23,7 @@ class AddMovie extends React.Component {
 
   render() {
     const movie = this.props;
-    const { title, subtitle, imagePath, storyline, rating } = movie;
+    const { title, subtitle, imagePath, storyline, rating, genre } = movie;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title" data-testid="title-input-label">
@@ -72,12 +72,21 @@ class AddMovie extends React.Component {
           Avaliação:
           <input 
           type="number"
-          value={ this.rating }
+          value= { this.rating }
           onChange={ rating }
           data-testid="rating-input"
           />
         </label>
 
+        <label data-testid="genre-input-label">
+          Gênero:
+          <select value={ this.genre } onChange={ genre } data-testid="genre-input">
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="genre-option" selected>Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
