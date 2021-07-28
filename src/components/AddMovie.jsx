487e444acc84ea5c2344e15b94cc9e0bd73f1646
Jonmorais <jonathan_ms15@hotmail.com>
2021-07-27 21:19:30 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   render() {
+    const { onClick } = this.props;
+
     return (
-      <h1>funciona</h1>
+      <div>
+        <form>
+          {onClick}
+        </form>
+      </div>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
