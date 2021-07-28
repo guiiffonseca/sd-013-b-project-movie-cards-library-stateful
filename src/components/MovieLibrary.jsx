13cae90bee4c279a97884dc173bda18c050a2888
LucianoJunior <lucianojunior4242@gmail.com>
2021-07-28 17:06:39 -0300

    Requisito 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 export default class MovieLibrary extends React.Component {
   constructor(props) {
@@ -40,6 +41,14 @@ export default class MovieLibrary extends React.Component {
     return filteredMovies;
   };
 
+  addNewMovie = (newMovie) => {
+    const { movies } = this.state;
+    const newMovies = [...movies, newMovie];
+    this.setState({
+      movies: newMovies,
+    });
+  };
+
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
@@ -55,6 +64,7 @@ export default class MovieLibrary extends React.Component {
         <MovieList
           movies={ this.getFilteredMovies(movies) }
         />
+        <AddMovie onClick={ this.addNewMovie } />
       </div>
     );
   }
