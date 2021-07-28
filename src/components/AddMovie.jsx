308ec689f5e0018ddcb2826982c4a7f2eb0c688d
Polyana Balotin <polybalotin@hotmail.com>
2021-07-28 14:35:41 -0300

    requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,53 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            value={ title }
+            onChange={ this.handleChange }
+            data-testid="title-input"
+            type="text"
+            name="title"
+          />
+        </label>
+
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+          Subtítulo
+          <input
+            onChange={ this.handleChange }
+            type="text"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            name="subtitle"
+          />
+        </label>
+      </form>
+
+    );
+  }
+}
+
+export default AddMovie;
