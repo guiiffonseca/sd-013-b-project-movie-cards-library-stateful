4140e6dc8f04a8878f58d2d0fefe764173e17db0
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-07-28 19:20:33 -0300

    RQ 1 - Criado componente SearchBar e props

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -6,9 +6,59 @@ import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   /*
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      onSearchTextChange: () => {},
+      bookmarkedOnly: false,
+      onBookmarkedChange: () => {},
+      selectedGenre: '',
+      onSelectedGenreChange: () => {},
+    };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    console.log(event.target.value);
+    this.setState({ searchText: event.target.value });
+  }
+
+  onBookmarkedChange(event) {
+    console.log(event.target.checked);
+    this.setState({ bookmarkedOnly: event.target.checked });
+  }
+
+  onSelectedGenreChange(event) {
+    console.log(event.target.checked);
+    this.setState({ selectedGenre: event.target.value });
   } */
+
+  /*
+  <section>
+        <input
+          value={ searchText }
+          type="text"
+          onChange={ this.onSearchTextChange }
+        />
+        <input
+          checked={ bookmarkedOnly }
+          type="checkbox"
+          onChange={ this.onBookmarkedChange }
+        />
+        <select>
+          <option
+            value={ selectedGenre }
+            onChange={ this.onSelectedGenreChange }
+          >
+            Todos
+          </option>
+        </select>
+      </section>
+  */
 
   render() {
     const { movies } = this.props;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,42 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.props;
+    const { onSearchTextChange, onBookmarkedChange, onSelectedGenreChange } = this.props;
+    return (
+      <section>
+        <input
+          value={ searchText }
+          type="text"
+          onChange={ onSearchTextChange }
+        />
+        <input
+          checked={ bookmarkedOnly }
+          type="checkbox"
+          onChange={ onBookmarkedChange }
+        />
+        <select>
+          <option
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            Todos
+          </option>
+        </select>
+      </section>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
