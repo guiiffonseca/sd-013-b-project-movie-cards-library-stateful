f3569a4f9fb3c4bff26383cecb7798e27b6c4d68
Gustavo Fernando <gu.bressan@outlook.com>
2021-07-28 15:53:01 -0700

    requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React from "react";
+
+class AddMovie extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      subtitle:"",
+      title:"",
+      imagePath:"",
+      storyline:"",
+      rating:0,
+      genre:"action",
+    };
+  }
+  render() {
+    const { onClick }=this.props;
+    return(
+      <form>{onClick}</form>
+    );
+  }
+}
+
+export default AddMovie;
