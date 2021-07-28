92d627aca7d2f80c09bc88b7bdb72d972a4a426c
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-07-28 01:19:09 -0300

    Requisitos 15 e 17 funcionando

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import movies from '../data';
 
 class MovieLibrary extends React.Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
