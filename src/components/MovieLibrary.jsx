a8a01d6b133a3cbc0f0332449ea09e9775364427
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-28 14:45:34 -0300

    requisito 15 - concluido

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,28 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <h1>MovieLibrary</h1>
+    )
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+      bookmarked: PropTypes.bool,
+      genre: PropTypes.string,
+    }),
+  ),
+};
+
+MovieLibrary.defaultProps = {
+  movies: [],
+};
