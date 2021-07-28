26ec183f59e824555d5aa12af2e6b3e9218a90d3
Guilherme <guilhermerg1234@gmail.com>
2021-07-28 17:59:21 -0300

    Ex3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,38 +1,36 @@
 // implement SearchBar component here
 import React from 'react';
 
-class Form extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Form</h1>
-                <form data-testid="search-bar-form">
-
-                </form>
-            </div>
-        )
-    }
-}
-
 const SearchBar = (props) => {
-        let searchText = props.searchText;
-        let onSearchTextChange = props.onSearchTextChange;
-        let bookmarkedOnly = props.bookmarkedOnly;
-        let onBookmarkedChange = props.onBookmarkedChange;
-        let selectedGenre = props.selectedGenre;
-        let onSelectedGenreChange = props.onSelectedGenreChange;
-
-        return (
-            <div>
+    let searchText = props.searchText;
+    let onSearchTextChange = props.onSearchTextChange;
+    let bookmarkedOnly = props.bookmarkedOnly;
+    let onBookmarkedChange = props.onBookmarkedChange;
+    let selectedGenre = props.selectedGenre;
+    let onSelectedGenreChange = props.onSelectedGenreChange;
+    
+    return (
+        <div>
                 {searchText}
                 {onSearchTextChange}
                 {bookmarkedOnly}
                 {onBookmarkedChange}
                 {selectedGenre}
                 {onSelectedGenreChange}
-                <Form />
+
+                <form data-testid="search-bar-form">
+                    <label data-testid="text-input-label">
+                    Inclui o texto:                
+                        <input 
+                            data-testid="text-input" 
+                            value={ searchText } 
+                            onChange={ onSearchTextChange }
+                            type="text">
+                        </input>
+                    </label>
+                </form>
             </div>
         )
     }
-
-export default SearchBar;
+    
+    export default SearchBar;
