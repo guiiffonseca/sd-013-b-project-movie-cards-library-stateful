7dff0941654b18fe9e262f775e479a8fff360d86
Alex <alexcorreas1994@gmail.com>
2021-07-28 20:00:06 -0300

    Renderizando MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,36 +1,136 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+    this.titleinputCallback = this.titleinputCallback.bind(this);
+    this.bookmarkCallback = this.bookmarkCallback.bind(this);
+    this.selectedGenreCallback = this.selectedGenreCallback.bind(this);
+    this.MovieCallback = this.MovieCallback.bind(this);
+    this.myFilter = this.myFilter.bind(this);
+    this.movieFilter = this.movieFilter.bind(this);
+    this.bookMarkedMovieFilter = this.bookMarkedMovieFilter.bind(this);
+  }
 
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
+  titleInputCallback(event) {
+    this.setState({ searchText: event.target.value });
+  }
 
-//     this.state = {
-//       searchText: '',
-//     };
+  bookMarkCallback(event) {
+    this.setState({ bookmarkedOnly
+    : event.target.checked });
+  }
 
-//     this.handleChange = this.handleChange.bind(this);
-//   }
+  selectedGenreCallback(event) {
+    this.setState({ selectedGenre: event.target.value });
+  }
 
-//   handleChange(event) {
-//     console.log(event.target.value);
-//     this.setState({ searchText: event.target.value });
-//   }
+  MovieCallback({ subtitle, title, imagePath, storyline, rating, genre, bookmarked }) {
+    const newMovie = { subtitle, title, imagePath, storyline, rating, genre, bookmarked };
+    console.log(newMovie);
+    this.setState((prevState) => ({
+      movies: [...prevState.movies, newMovie],
+    }));
+  }
 
-//   render() {
-//     const { searchText } = this.state;
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar searchText={ this.handleChange } />
-//         <MovieList movies={ this.state.movies } />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+  movieFilter() {
+    const { searchText, selectedGenre, movies } = this.state;
+    if (selectedGenre !== '') {
+      if (searchText !== '') {
+        console.log(`todos os filmes
+            com o genero ${selectedGenre} e o texto ${searchText}`);
+        return movies.filter((movie) => movie.genre === selectedGenre)
+          .filter((movie) => (
+            movie.title.toLowerCase().includes(searchText.toLowerCase())
+          || movie.subtitle.toLowerCase().includes(searchText.toLowerCase())
+          || movie.storyline.toLowerCase().includes(searchText.toLowerCase())));
+      }
+      console.log(`todos os filmes com o genêro ${selectedGenre}`);
+      return movies.filter((movie) => movie.genre === selectedGenre);
+    }
+    if (searchText !== '') {
+      console.log(`todos os filmes  com o texto ${searchText}`);
+      return movies.filter((movieAll) => (
+        movieAll.title.toLowerCase().includes(searchText.toLowerCase())
+        || movieAll.subtitle.toLowerCase().includes(searchText.toLowerCase())
+        || movieAll.storyline.toLowerCase().includes(searchText.toLowerCase())));
+    }
+    console.log('todos os filmes');
+    return movies;
+  }
 
-// export default MovieLibrary;
+  bookMarkedMovieFilter() {
+    const { searchText, selectedGenre, movies } = this.state;
+    if (selectedGenre !== '') {
+      if (searchText !== '') {
+        console.log(`todos os filmes
+        favoritos com o genêro ${selectedGenre} e o texto ${searchText}`);
+        return movies.filter((movie) => movie.bookMarked === true)
+          .filter((movie) => (
+            movie.storyline.toLowerCase().includes(searchText.toLowerCase())
+        || movie.subtitle.toLowerCase().includes(searchText.toLowerCase())
+        || movie.title.toLowerCase().includes(searchText.toLowerCase())));
+      }
+      console.log(`todos os filmes favoritos com o genêro ${selectedGenre}`);
+      return movies.filter((movie) => movie.bookMarked === true)
+        .filter((movie) => movie.genre === selectedGenre);
+    }
+    if (searchText !== '') {
+      console.log(`todos os filmes favoritos com o texto ${searchText}`);
+      return movies.filter((movie) => movie.bookMarked === true)
+        .filter((movie) => (
+          movie.title.toLowerCase().includes(searchText.toLowerCase())
+            || movie.subtitle.toLowerCase().include(searchText.toLowerCase())
+            || movie.storyline.toLowerCase().includes(searchText.toLowerCase())));
+    }
+  }
+
+  myFilter() {
+    const { searchText, selectedGenre, movies, bookmarkedOnly } = this.state;
+    if (bookmarkedOnly === true) {
+      return this.bookMarkedMovieFilter();
+    }
+    if (!searchText || !selectedGenre) {
+      return this.movieFilter();
+    }
+    if (searchText === '' && selectedGenre === '') {
+      return movies;
+    }
+  }
+
+  render() {
+    const { searchText, selectedGenre, bookmarkedOnly } = this.state;
+    const movies = this.myFilter();
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.titleInputCallback }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.bookmarkCallback }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.selectedGenreCallback }
+        />
+        <AddMovie
+          onClick={ this.MovieCallback }
+        />
+        <MovieList movies={ movies } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
