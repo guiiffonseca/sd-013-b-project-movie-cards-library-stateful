060607f994621952fe2a29ed8aabd224b2ace55a
Thaís Sampaio <thais_castrocs@outlook.com>
2021-07-28 15:01:55 -0300

    Requisitos 8 e 9 Realizados

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,12 +13,40 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    this.setState({
+      title: event.target.value,
+    });
   }
 
   render() {
-    const { onClick } = this.props;
+    const { title, subtitle } = this.props;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="Input-Title">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            value={ title }
+            onChange={ this.handleInput }
+            id="Input-Title"
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="Input-Subtitle">
+          Subtítulo
+          <input
+            data-testid="subtitle-input"
+            type="text"
+            value={ subtitle }
+            id="Input-Subtitle"
+            onChange={ this.handleInput }
+          />
+        </label>
+      </form>
     );
   }
 }
