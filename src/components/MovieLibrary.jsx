c0a60670db840725913dd3d0173e0044afc9a5b8
Arthur-Jr <arthurjr1906@gmail.com>
2021-07-27 21:49:27 -0300

    Adiciona o décimo quinto e o décimo sexto requisito.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,48 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
 
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies
+    }
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={this.props.movies} />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+  render() {
 
-// export default MovieLibrary;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ this.state.movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        imagePath: PropTypes.string.isRequired,
+        bookmarked: PropTypes.bool.isRequired,
+        genre: PropTypes.string.isRequired,
+      },
+    ),
+  ).isRequired,
+};
+
+export default MovieLibrary;
