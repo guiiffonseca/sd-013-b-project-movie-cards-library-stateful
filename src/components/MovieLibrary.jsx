1e276f19cd97c9575e0122a8ed44430b1990ed0b
Luishsk93 <hyungsunkangkr@gmail.com>
2021-07-28 00:12:15 -0300

    Corrigindo lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -26,19 +26,19 @@ export default class MovieLibrary extends Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({ [name]: value });
 
-    // if (bookmarkedOnly === false) {
-    //   this.setState({ movies: movies.filter((movie) => movie.bookmarked === true) });
-    // } else if (searchText === '') {
-    //   console.log('a');
-    //   shownMovies = movies.filter((movie) => movie.title.toLowerCase()
-    //     .includes(searchText.toLowerCase()) || movie.subtitle.toLowerCase()
-    //     .includes(searchText.toLowerCase()) || movie.storyline.toLowerCase()
-    //     .includes(searchText.toLowerCase()));
-    //   this.setState({ movies: shownMovies });
-    // } else if (selectedGenre === '') {
-    //   shownMovies = movies.filter((movie) => movie.genre === selectedGenre);
-    //   this.setState({ movies: shownMovies });
-    // }
+    if (bookmarkedOnly === false) {
+      this.setState({ movies: movies.filter((movie) => movie.bookmarked === true) });
+    } else if (searchText === '') {
+      console.log('a');
+      shownMovies = movies.filter((movie) => movie.title.toLowerCase()
+        .includes(searchText.toLowerCase()) || movie.subtitle.toLowerCase()
+        .includes(searchText.toLowerCase()) || movie.storyline.toLowerCase()
+        .includes(searchText.toLowerCase()));
+      this.setState({ movies: shownMovies });
+    } else if (selectedGenre === '') {
+      shownMovies = movies.filter((movie) => movie.genre === selectedGenre);
+      this.setState({ movies: shownMovies });
+    }
   }
 
   render() {
