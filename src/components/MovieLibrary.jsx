8e0b5794b5f304d632bf892b3798173fd9281d32
Arthur-Jr <arthurjr1906@gmail.com>
2021-07-27 23:11:00 -0300

    Adiciona o décimo sétimo e o décimo oitavo requisito.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -8,21 +8,87 @@ class MovieLibrary extends Component {
   constructor(props) {
     super(props);
 
+    const { movies } = this.props;
+
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: this.props.movies
+      movies,
+    };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange({ target }) {
+    const { movies } = this.props;
+    const { value } = target;
+
+    const filter = movies.filter((movie) => (
+      movie.title.includes(value) || movie.subtitle.includes(value)
+      || movie.storyline.includes(value)));
+
+    this.setState({
+      searchText: value,
+      movies: filter,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    const { checked } = target;
+    const { movies } = this.props;
+
+    const filter = movies.filter((movie) => movie.bookmarked === checked);
+
+    if (checked) {
+      this.setState({
+        bookmarkedOnly: checked,
+        movies: filter,
+      });
+    } else {
+      this.setState({
+        bookmarkedOnly: checked,
+        movies,
+      });
+    }
+  }
+
+  onSelectedGenreChange({ target }) {
+    const { value } = target;
+    const { movies } = this.props;
+
+    const filter = movies.filter((movie) => movie.genre.includes(value));
+
+    if (value === '') {
+      this.setState({
+        selectedGenre: 'Todos',
+        movies,
+      });
+    } else {
+      this.setState({
+        selectedGenre: value,
+        movies: filter,
+      });
     }
   }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
 
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={ this.state.movies } />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
