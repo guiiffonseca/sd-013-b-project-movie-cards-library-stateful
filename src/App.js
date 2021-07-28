17c66272381ae811dcd71d5bbed75ad9ba9ff6ba
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-07-28 18:21:45 -0300

    lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 // import SearchBar from './Components/SearchBar';
-import MovieList from './components/MovieList';
+import data from './data';
 import './App.css';
+import MovieList from './components/MovieList';
+// import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList />
+      <MovieList movies={ data } />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,10 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class AddMovie extends React.Component {
+    constructor() {
+        super()
+    }
+}
