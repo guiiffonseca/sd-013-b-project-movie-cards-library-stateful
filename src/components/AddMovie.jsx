3d2eb53da2835cf00e62dda7e75f8b0cbb636ece
Polyana Balotin <polybalotin@hotmail.com>
2021-07-28 15:09:57 -0300

    requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -82,6 +82,20 @@ class AddMovie extends Component {
             data-testid="rating-input"
           />
         </label>
+
+        <label data-testid="genre-input-label" htmlFor="genre-input">
+          Gênero
+          <select
+            name="genre"
+            value={ genre }
+            onChange={ this.handleChange }
+            data-testid="genre-input"
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
