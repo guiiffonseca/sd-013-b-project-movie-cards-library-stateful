92488fa3f89992178740ff13eae276e131c21f67
mateusparr <mateus.dji@icloud.com>
2021-07-28 10:02:09 -0300

    project start requisito 1 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
+// import movies from './data';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenre } = this.props;
+
+    return (
+      <span>Olá</span>
+    );
+  }
+}
+
+export default SearchBar;
