bfc8317ad5e1e44b0e289534a0e6ece917f6e842
Aleilton Chavenco <aleilton@gmail.com>
2021-07-27 21:46:20 -0300

    Requisito 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -24,7 +24,7 @@ class AddMovie extends React.Component {
 
   render() {
     const { onClick } = this.props;
-    const { title, subtitle, imagePath } = this.state;
+    const { title, subtitle, imagePath, storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title-input" data-testid="title-input-label">
@@ -54,6 +54,15 @@ class AddMovie extends React.Component {
             value={ imagePath }
             data-testid="image-input"
             name="imagePath"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            name="storyline"
+            value={ storyline }
+            data-testid="storyline-input"
             onChange={ this.handleChange }
           />
         </label>
