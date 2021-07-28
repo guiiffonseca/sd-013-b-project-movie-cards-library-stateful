df769c14798f332fabea48a5ed6b7e9638aa62bc
Mayu <mayusatori@gmail.com>
2021-07-28 00:29:37 -0300

    Alerta de commit bomba: esse commit resolve a maior parte dos requisitos de uma vez só, a culpa é do projeto que não me deixa comita enquanto não estiver tudo funcionando. Só está faltando o componente AddMovie

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,76 @@
 // implement MovieLibrary component here a
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    this.setState({
+      searchText: event.target.value,
+    });
+  }
+
+  onBookmarkedChange(event) {
+    console.log(event.target.checked);
+    this.setState({
+      bookmarkedOnly: event.target.checked,
+    });
+  }
+
+  onSelectedGenreChange(event) {
+    this.setState({
+      selectedGenre: event.target.value,
+    });
+  }
+
+  render() {
+    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const moviesResult = movies
+      .filter(({ title, subtitle, storyline }) => title
+        .includes(searchText) + subtitle
+        .includes(searchText) + storyline
+        .includes(searchText))
+      .filter(({ genre }) => genre.includes(selectedGenre))
+      .filter(({ bookmarked }) => {
+        if (bookmarkedOnly === false) {
+          return true;
+        }
+        return bookmarked === bookmarkedOnly;
+      });
+    return (
+      <>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ moviesResult } />
+      </>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
