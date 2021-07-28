c71a457e14aa3f6e10e13cf3aa98882abd8a8fb2
FilipeGuto <filipeguto95@gmail.com>
2021-07-28 00:40:26 -0300

    Exercise 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,37 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
 
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={ this.props.movies } />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+    this.state = {
+      // searchText: '',
+      // bookmarkedOnly: false,
+      // selectdGenre: '',
+      movies: props.movies,
+    };
+  }
 
-// export default MovieLibrary;
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+export default MovieLibrary;
