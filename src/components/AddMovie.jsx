0ca615755b5ac9712e830c7e8cf76367076161d5
Jeferson Oliveira <jeferson.contas@gmail.com>
2021-07-28 01:54:49 -0300

    safe

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import AddTitle from './AddTitle';
+import AddSubtitle from './AddSubtitle';
+import AddImage from './AddImage';
+import AddStoryline from './AddStoryline';
+import AddRating from './AddRating';
+import AddGenre from './AddGenre';
 
 class AddMovie extends React.Component {
   constructor(props) {
@@ -22,7 +29,7 @@ class AddMovie extends React.Component {
       [name]: value,
     });
   }
-  
+
   onClickButton() {
     const { onClick } = this.props;
     onClick(this.state);
@@ -36,82 +43,19 @@ class AddMovie extends React.Component {
     });
   }
 
-
-
   render() {
     const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
-          Título
-          <input
-            type="text"
-            name="title-input"
-            id="tile-input"
-            value={ title }
-            data-testid="title-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            type="text"
-            name="subtitle-input"
-            id="subtitle-input"
-            value={ subtitle }
-            data-testid="subtitle-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="image-input" data-testid="image-input-label">
-          Imagem
-          <input
-            type="text"
-            name="image-input"
-            id="image-input"
-            value={ imagePath }
-            data-testid="image-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="storyline-input" data-testid="storyline-input-label">
-          Sinopse
-          <input
-            type="text"
-            name="storyline-input"
-            id="storyline-input"
-            value={ storyline }
-            data-testid="storyline-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="rating-input" data-testid="rating-input-label">
-          Avaliação
-          <input
-            type="number"
-            name="rating-input"
-            id="rating-input"
-            value={ rating }
-            data-testid="rating-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="genre-input" data-testid="genre-input-label">
-          Gênero
-          <select
-            value={ genre }
-            onChange={ this.handleChange }
-            data-testid="genre-input"
-          >
-            <option value="action" data-testid="genre-option">Ação</option>
-            <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="thriller" data-testid="genre-option">Suspense</option>
-          </select>
-        </label>
+        <AddTitle value={ title } handleChange={ this.handleChange } />
+        <AddSubtitle value={ subtitle } handleChange={ this.handleChange } />
+        <AddImage value={ imagePath } handleChange={ this.handleChange } />
+        <AddStoryline value={ storyline } handleChange={ this.handleChange } />
+        <AddRating value={ rating } handleChange={ this.handleChange } />
+        <AddGenre value={ genre } handleChange={ this.handleChange } />
         <button
-          type="button" 
+          type="button"
           data-testid="send-button"
           onClick={ this.onClickButton }
         >
@@ -121,5 +65,9 @@ class AddMovie extends React.Component {
     );
   }
 }
-()
+
 export default AddMovie;
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,14 +7,38 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+    this.handleChangeLibrary = this.handleChangeLibrary.bind(this);
+  }
+
+  handleChangeLibrary({ target }) {
+    const { name, value } = target;
+    const value = target.type === 'checkbox' ? target.checked: target.value;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <SearchBar
+        searchText={ searchText }
+        onSearchTextChange={ this.onSearchTextChange }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ this.onBookmarkedChange }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
@@ -22,3 +46,7 @@ class MovieLibrary extends Component {
 }
 
 export default MovieLibrary;
+
+MovieLibrary.propTypes = {
+  value: PropTypes.string.isRequired,
+};
