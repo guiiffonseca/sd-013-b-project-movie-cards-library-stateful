7b1cf3274906646b679ca16b54a7ae983b4c4d67
mateusparr <mateus.dji@icloud.com>
2021-07-28 16:16:20 -0300

    requisito 8 done

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      title: target.value,
+    });
+  }
+
   render() {
     const {
       subtitle,
@@ -12,7 +27,17 @@ class AddMovie extends React.Component {
     } = this.props;
 
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
