b1a18cd9e8bccc7e7cb5bb928044db8dbf1bf23a
marcus <marcus.rodrigues10@hotmail.com>
2021-07-27 21:36:04 -0300

    exercise 15, 16 and 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,33 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import movies from '../data';
+import SearchBar from './SearchBar';
+// import MovieList from './MovieList';
+
+export default class MovieLibrary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: { movies },
+    };
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <SearchBar />
+    );
+  }
+}
