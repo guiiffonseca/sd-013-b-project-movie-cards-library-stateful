ef2ce114fe79965a14d59208bb3904503c9ce6b1
unknown <sergio.espp@gmail.com>
2021-07-28 18:50:59 -0300

    Requisitos teste

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,35 @@
-// implement MovieLibrary component here
+import React from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { movies } = this.props;
+    return (
+      <>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <AddMovie movies={ movies } />
+      </>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
