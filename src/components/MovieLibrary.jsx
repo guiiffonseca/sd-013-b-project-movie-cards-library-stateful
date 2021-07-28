6ce7e647b8b8495ffd3faf0c24f2b0a2b6b713f0
guiiffonseca <guii.ffonseca22@gmail.com>
2021-07-28 19:07:34 -0300

    Requisto 15 e 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,27 +1,32 @@
-/* import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-   constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies:'',
-    }
+    };
   }
 
   render() {
-    const {movies} = this.props;
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
         <MovieList movies={ movies } />
         <AddMovie />
       </div>
@@ -29,4 +34,18 @@ class MovieLibrary extends Component {
   }
 }
 
-export default MovieLibrary;  */
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      storyline: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+      imagePath: PropTypes.string.isRequired,
+      bookmarked: PropTypes.bool.isRequired,
+      genre: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default MovieLibrary;
