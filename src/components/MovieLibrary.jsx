e6f937231850e91984f7915ea40ff586d0fc48fd
diogodlemos <diogodrummond@hotmail.com>
2021-07-27 22:48:53 -0300

    Componente <AddMovie /> finalizado

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -11,22 +12,20 @@ class MovieLibrary extends Component {
 //   }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar
-          searchText={ searchText }
-          onSearchTextChange={ onSearchTextChange }
-          onBookmarkedChange={ onBookmarkedChange }
-          bookmarkedOnly={ bookmarkedOnly }
-          selectedGenre={ selectedGenre }
-          onSelectedGenreChange={ onSelectedGenreChange }
-        />
+        <SearchBar />
         <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
