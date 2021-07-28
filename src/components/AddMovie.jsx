eafb145fb5375c83d2902efeaa7c2abbe0f9225f
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-07-28 14:02:23 -0300

    requisito 15

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Genre from './components/Genre';
 import RatingInput from './components/RatingInput';
 import Storyline from './components/Storyline';
@@ -11,6 +12,7 @@ class AddMovie extends React.Component {
     super();
 
     this.handleChange = this.handleChange.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
 
     this.state = {
       title: '',
@@ -29,21 +31,63 @@ class AddMovie extends React.Component {
     });
   }
 
+  onButtonClick= (event) => {
+    event.preventDefault();
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
     // const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <Title handleChange={ this.handleChange } title={ title } />
-        <Subtitle handleChange={ this.handleChange } subtitle={ subtitle } />
-        <Image handleChange={ this.handleChange } imagePath={ imagePath } />
-        <Storyline handleChange={ this.handleChange } storyline={ storyline } />
-        <RatingInput handleChange={ this.handleChange } rating={ rating } />
-        <Genre handleChange={ this.handleChange } genre={ genre } />
-        <button type="button" data-testid="send-button">Adicionar Filme</button>
+        <Title
+          handleChange={ this.handleChange }
+          title={ title }
+        />
+        <Subtitle
+          handleChange={ this.handleChange }
+          subtitle={ subtitle }
+        />
+        <Image
+          handleChange={ this.handleChange }
+          imagePath={ imagePath }
+        />
+        <Storyline
+          handleChange={ this.handleChange }
+          storyline={ storyline }
+        />
+        <RatingInput
+          handleChange={ this.handleChange }
+          rating={ rating }
+        />
+        <Genre
+          handleChange={ this.handleChange }
+          genre={ genre }
+        />
+        <button
+          type="submit"
+          data-testid="send-button"
+          onClick={ this.onButtonClick }
+        >
+          Adicionar filme
+        </button>
       </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,29 +1,32 @@
-// import React from 'react';
-// import AddMovie from './AddMovie';
-// import SearchBar from './SearchBar';
+import React from 'react';
+// import PropTypes from 'prop-types';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
 // import MovieList from './MovieList';
 
-// class MovieLibrary extends React.Component {
-//   constructor(props) {
-//     super();
+class MovieLibrary extends React.Component {
+  // constructor() {
+  //   super();
 
-//     this.state = {
-//       searchText: '',
-//       bookmarkedOnly: false,
-//       selectedGenre: '',
-//       movies: props.movies,
-//     };
-//   }
+  //   // this.state = {
+  //   //   searchText: '',
+  //   //   bookmarkedOnly: false,
+  //   //   selectedGenre: '',
+  //   //   movies: props.movies,
+  //   // };
+  // }
 
-//   render() {
-//     const { movies } = this.props;
-//     return (
-//       <div>
-//         <SearchBar />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
 
-// export default MovieLibrary;
+MovieLibrary.propTypes = {
+};
+
+export default MovieLibrary;
