98cfdb21b1ab2971dbd1d91a38c4f20da480f8d8
Yg.Or <inygu@icloud.com>
2021-07-28 16:55:05 -0300

    requisitos 19

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import './App.css';
 import MovieLibrary from './components/MovieLibrary';
 import Movies from './data';
 
-function alteraFilmes(novos){
-  Movies.push(novos)
-  console.log(Movies)
+function alteraFilmes(novos) {
+  Movies.push(novos);
 }
 
 function App() {
   return (
     <div id="App" className="App">
       <Header />
-      <MovieLibrary movies={ Movies } funcao={alteraFilmes}/>
+      <MovieLibrary movies={ Movies } funcao={ alteraFilmes }/>
     </div>
   );
 }
