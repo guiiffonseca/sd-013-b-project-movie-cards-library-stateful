3d213fa8b7cb51cb93eaeabe9e8d707ba82244e8
bvottri <bruna.votre@gmail.com>
2021-07-28 18:41:09 -0300

    Requisito 7 ao 14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 class AddMovie extends React.Component {
   constructor() {
     super();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       subtitle: '',
       title: '',
@@ -13,10 +15,104 @@ class AddMovie extends React.Component {
     };
   }
 
-  render() {
+  handleClick() {
     const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
     return (
-      <form action="" />
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            name="title"
+            data-testid="title-input"
+            type="text"
+            id="title-input"
+            value={ this.state.title }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+          Subtítulo
+          <input
+            name="subtitle"
+            data-testid="subtitle-input"
+            type="text"
+            id="subtitle-input"
+            value={ this.state.subtitle }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="image-input-label" htmlFor="image-input">
+          Imagem
+          <input
+            name="imagePath"
+            data-testid="image-input"
+            type="text"
+            value={ this.state.imagePath }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="storyline-input-label" htmlFor="storyline-input">
+          Sinopse
+          <textarea
+            name="storyline"
+            data-testid="storyline-input"
+            id="storyline-input"
+            value={ this.state.storyline }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="rating-input-label" htmlFor="rating-input">
+          Avaliação
+          <input
+            name="rating"
+            data-testid="rating-input"
+            type="number"
+            value= { this.state.rating }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="genre-input-label" htmlFor="genre-input">
+          Gênero
+          <select
+            name="genre"
+            data-testid="genre-input"
+            id="genre-input"
+            value= { this.state.genre }
+            onChange={ this.handleChange }
+            >
+              <option data-testid="genre-option" value="action">Ação</option>
+              <option data-testid="genre-option" value="comedy">Comédia</option>
+              <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+        <button
+          type="button"
+          data-testid="send-button"
+          onClick={ this.handleClick }
+        >
+          Adicionar filme
+        </button>
+      </form>
     );
   }
 }
