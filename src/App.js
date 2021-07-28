a932fff0a6f91d612fe232cc909dd0036b4bc6ec
Yg.Or <inygu@icloud.com>
2021-07-28 14:30:37 -0300

    requisito 14

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <SeacrhBar
         searchText=""
-        onSearchTextChange=""
+        onSearchTextChange=" "
         bookmarkedOnly=""
         onBookmarkedChange=""
       />
