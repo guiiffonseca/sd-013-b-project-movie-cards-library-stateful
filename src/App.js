38f975a65a264105aa2ccede4a822434aeff9131
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-07-28 19:47:25 -0300

    conclui requisitos 15

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import './App.css';
 import AddMovie from './components/AddMovie';
+import movies from './data';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   const onClick = (param) => {
@@ -16,6 +18,7 @@ function App() {
       <Header />
       <SearchBar />
       <AddMovie onClick={ onClick } />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,20 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const {
+      movies,
+    } = this.props;
+    return (
+      <div>{ movies[0].title }</div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.objectOf(PropTypes.movies)
+    .isRequired,
+};
+
+export default MovieLibrary;
