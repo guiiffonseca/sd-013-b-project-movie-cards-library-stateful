15f51acdf52a3c0a0a03bfdc5f521ac3a01df02c
EddieRAM <edusacarneiro@gmail.com>
2021-07-28 19:46:20 -0300

    Requisites 6, 7 e 8

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import MovieList from './components/MovieList';
 import movies from './data';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <AddMovie />
       <MovieList movies={ movies } />
     </div>
   );
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    /* this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }; */
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.props;
     return (
-      console.log('feliz')
+      <section>
+        <form className="form" data-testid="add-movie-form">
+          <label htmlFor="title-input" data-testid="title-input-label">
+            Título
+            <input type="text" value={ title } data-testid="title-input" />
+          </label>
+        </form>
+      </section>
     );
   }
 }
+
+AddMovie.propTypes = ({
+  subtitle: PropTypes.string,
+  title: PropTypes.string,
+  imagePath: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  genre: PropTypes.string,
+}).isRequired;
 
 export default AddMovie;
