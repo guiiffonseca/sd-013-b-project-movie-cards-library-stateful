62b8eeab789e3f17e7163ffe62363b0d9e6e1767
LucianoJunior <lucianojunior4242@gmail.com>
2021-07-28 14:38:49 -0300

    Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
   render() {
     return '';
   }
