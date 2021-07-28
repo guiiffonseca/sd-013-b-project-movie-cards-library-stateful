3649be77b58fd1fedfa5bb646b7a2e679a1ac5c3
marcus <marcus.rodrigues10@hotmail.com>
2021-07-28 15:16:11 -0300

    exercise 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import movies from '../data';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
-// import MovieList from './MovieList';
+import MovieList from './MovieList';
 
 export default class MovieLibrary extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: { movies },
+      movies: props.movies,
     };
     this.updateState = this.updateState.bind(this);
+  }
+
+  filterMovies() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    movies.filter((movie) => (bookmarkedOnly ? movie.bookmarkedOnly : movie));
+    movies.filter((movie) => (movie.title.includes(searchText)
+    || movie.subtitle.includes(searchText) || movie.storyline.includes(searchText)));
+    const filterGenre = movies
+      .filter((movie) => (selectedGenre === '' ? movie.genre : movie));
+    return filterGenre;
   }
 
   updateState({ target }) {
@@ -25,9 +35,31 @@ export default class MovieLibrary extends Component {
   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      <SearchBar />
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.updateState }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.updateState }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.updateState }
+        />
+        <MovieList movies={ this.filterMovies() } />
+      </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+};
