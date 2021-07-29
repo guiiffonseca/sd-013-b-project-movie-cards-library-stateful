2ccd38c8fcc2689caadb7d8e9220bde97cd876cd
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-07-28 21:24:56 -0300

    RQ 6 - Criado componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  /*
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      subtitle: '',
+      storyline: '',
+      rating: 0,
+      imagePath: '',
+      genre: 'action',
+    }
+  } */
+  render() {
+    const { callBack } = this.props;
+    return (
+      <form>
+        <input type="text" onClick={ callBack } />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  callBack: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
