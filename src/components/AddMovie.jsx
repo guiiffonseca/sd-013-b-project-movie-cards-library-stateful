8606e2dd69b67c759e9ce87e73660e388ff64372
EddieRAM <edusacarneiro@gmail.com>
2021-07-28 23:34:28 -0300

    Requisite 12-13 done, but need to solve lint issues

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-  /* constructor() {
+  constructor() {
     super();
     this.state = {
       subtitle: '',
@@ -12,42 +12,49 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
-  } */
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
 
   render() {
-    const { subtitle,
-      title,
-      imagePath,
-      storyline,
-      /* rating,
-       genre */
-    } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-      <section>
-        <form className="form" data-testid="add-movie-form">
-          <label htmlFor="title-input" data-testid="title-input-label">
-            Título
-            <input type="text" value={ title } data-testid="title-input" />
-          </label>
-          <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
-            Subtítulo
-            <input type="text" value={ subtitle } data-testid="subtitle-input" />
-          </label>
-          <label htmlFor="image-input" data-testid="image-input-label">
-            Imagem
-            <input value={ imagePath } data-testid="image-input" />
-          </label>
-          <label htmlFor="storyline-input" data-testid="storyline-input-label">
-            Sinopse
-            <input
-              data-testid="storyline-input"
-              type="text"
-              name="storyline"
-              value={ storyline }
-            />
-          </label>
-        </form>
-      </section>
+      <form className="form" data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input type="text" name="title" value={ title } data-testid="title-input" onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" name="subtitle" value={ subtitle } data-testid="subtitle-input" onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input data-testid="image-input" name="imagePath" value={ imagePath } onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          <input data-testid="storyline-input" type="text" name="storyline" value={ storyline } onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="rating-input" data-testid="rating-input-label">
+          Avaliação
+          <input type="number" name="rating" value={ rating } data-testid="rating-input" onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="genre-input" data-testid="genre-input-label">
+          Gênero
+          <select name="genre" value={ genre } data-testid="genre-input" onChange={ this.handleChange }>
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
@@ -59,6 +66,7 @@ AddMovie.propTypes = ({
   storyline: PropTypes.string,
   rating: PropTypes.number,
   genre: PropTypes.string,
+  onClick: PropTypes.func,
 }).isRequired;
 
 export default AddMovie;
