ffc2497e423c475d92a70894332731d4734d8ad2
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-29 17:34:54 -0300

    req 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,22 @@
-// implement AddMovie component here
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function AddMovie(props) {
+  const { onClick } = props;
+  const [subtitle, setSubtitle] = useState('');
+  const [title, setTitle] = useState('');
+  const [imagePath, setImagePath] = useState('');
+  const [storyLine, setStoryline] = useState('');
+  const [rating, setRating] = useState(0);
+  const [genre, setGenre] = useState('action');
+
+  return (
+    <h1>teste</h1>
+  );
+}
+
+export default AddMovie;
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
