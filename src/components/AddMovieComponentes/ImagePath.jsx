858245ee251a58eb6ae4d1831d6ab945e1b96551
luis carlos <luizcruz0809@gmail.com>
2021-07-28 23:14:56 -0300

    add 10

--- src/components/AddMovieComponentes/ImagePath.jsx
+++ src/components/AddMovieComponentes/ImagePath.jsx
@@ -4,14 +4,15 @@ class ImagePath extends React.Component {
   render() {
     const { state, evento } = this.props;
     return (
-      <label htmlFor="imagePath">
-        URL da imagem:
+      <label htmlFor="imagePath" data-testid="image-input-label">
+        Imagem
         <input
           id="imagePath"
           value={ state }
           onChange={ evento }
           name="imagePath"
           type="text"
+          data-testid="image-input"
         />
       </label>
     );
