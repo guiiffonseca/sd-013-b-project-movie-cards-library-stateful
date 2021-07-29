a5ac9caf8cc2a381f248fa22f044db7b72d7ae86
luis carlos <luizcruz0809@gmail.com>
2021-07-28 23:49:47 -0300

    add 13

--- src/components/AddMovieComponentes/Genre.jsx
+++ src/components/AddMovieComponentes/Genre.jsx
@@ -5,15 +5,20 @@ class Genre extends React.Component {
   render() {
     const { state, evento } = this.props;
     return (
-      <label htmlFor="genre">
-        Gênero:
-        <input
+      <label htmlFor="genre" data-testid="genre-input-label">
+        Gênero
+        <select
           id="genre"
           value={ state }
           onChange={ evento }
           name="genre"
           type="text"
-        />
+          data-testid="genre-input"
+        >
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
       </label>
     );
   }
