6cd51aad66db75b18f82de5a070148a025080471
k4nl <gustavoh.bh@gmail.com>
2021-07-29 12:04:25 -0300

    requisito 1 e 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,48 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="searchText">
+          Search:
+          <input
+            type="text"
+            id="searchText"
+            name="searchText"
+          />
+        </label>
+
+        <label htmlFor="bookMark">
+          Book:
+          <input
+            type="checkbox"
+            id="bookMark"
+            name="bookmarkedOnly"
+          />
+        </label>
+
+        <label htmlFor="select">
+          Select Genre:
+          <input
+            type="select"
+            id="select"
+            name="selectedGenre"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,64 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <form className="search-bar" data-testid="search-bar-form">
+
+        <label htmlFor="searchText">
+          Search:
+          <input
+            type="text"
+            id="searchText"
+            name="searchText"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+
+        <label htmlFor="bookMark">
+          Book:
+          <input
+            type="checkbox"
+            id="bookMark"
+            name="bookmarkedOnly"
+            value={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+
+        <label htmlFor="select">
+          Select Genre:
+          <input
+            type="radio"
+            id="select-input"
+            name="selectedGenre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
