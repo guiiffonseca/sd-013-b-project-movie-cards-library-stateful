e9d9266c9f7c942276678b7a4baa55551e8b3dc9
Jonmorais <jonathan_ms15@hotmail.com>
2021-07-30 09:34:01 -0300

    Requisitos 8 + 9 - setState

--- src/components/SubtitleTextInput.jsx
+++ src/components/SubtitleTextInput.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 class SubitleTextInput extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+    };
+    this.handleChangeOnSubtitle = this.handleChangeOnSubtitle.bind(this);
+  }
+
+  handleChangeOnSubtitle = (event) => {
+    this.setState({
+      subtitle: event.target.value,
+    });
+    console.log(this);
+  };
+
   render() {
+    const { subtitle } = this.state;
+
     return (
       <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
         Subtítulo
@@ -9,6 +26,8 @@ class SubitleTextInput extends React.Component {
           type="text"
           data-testid="subtitle-input"
           id="subtitle-input"
+          value={ subtitle }
+          onChange={ this.handleChangeOnSubtitle }
         />
       </label>
     );
--- src/components/TitleTextInput.jsx
+++ src/components/TitleTextInput.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 class TitleTextInput extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+    this.handleChangeOnTitle = this.handleChangeOnTitle.bind(this);
+  }
+
+  handleChangeOnTitle = (event) => {
+    this.setState({
+      title: event.target.value,
+    });
+    console.log(this);
+  };
+
   render() {
+    const { title } = this.state;
+
     return (
       <label htmlFor="title-input" data-testid="title-input-label">
         Título
@@ -9,6 +26,8 @@ class TitleTextInput extends React.Component {
           type="text"
           data-testid="title-input"
           id="title-input"
+          value={ title }
+          onChange={ this.handleChangeOnTitle }
         />
       </label>
     );
