7f5b38bc12f0b64c7872ea7575b2ea27bb323ba7
mateusparr <mateus.dji@icloud.com>
2021-07-30 17:09:09 -0300

    project update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
 import './App.css';
+// import MovieLibrary from './components/MovieLibrary';
 
 class App extends React.Component {
   render() {
