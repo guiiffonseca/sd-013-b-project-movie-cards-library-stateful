12bb5ee99bed69f6bfbbe1c39a25522f92c7a03d
Alexandre Altgauzen <altgauzen@gmail.com>
2021-07-30 14:42:11 +0100

    componentizaçao dos inputs refat1.1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -12,7 +12,7 @@ class MovieLibrary extends Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: props.movies,
+      // movies: props.movies,
     };
 
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
