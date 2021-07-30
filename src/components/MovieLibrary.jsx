0a570c4f7c120f859200246637c1b42f37fbe511
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-30 19:33:40 -0300

    req 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 function MovieLibrary(props) {
   const { movies } = props;
@@ -7,8 +9,43 @@ function MovieLibrary(props) {
   const [bookmarkedOnly, setbookmarkedOnly] = useState(false);
   const [selectedGenre, setSelectedGenre] = useState('');
   return (
-    <SearchBar />
+    <>
+      <SearchBar
+        searchText={ searchText }
+        onSearchTextChange={ (event) => setSearchText(event.target.value) }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ () => setbookmarkedOnly(!bookmarkedOnly) }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ (event) => setSelectedGenre(event.target.value) }
+      />
+      <MovieList
+        movies={ movies.filter(
+          (movie) => {
+            if (bookmarkedOnly === movie.bookmarked && movie.genre.includes(selectedGenre)) {
+              return movie.title.toLowerCase().includes(searchText.toLowerCase())
+                || movie.subtitle.toLowerCase().includes(searchText.toLowerCase())
+                || movie.storyline.toLowerCase().includes(searchText.toLowerCase());
+            }
+            if (bookmarkedOnly === false && movie.genre.includes(selectedGenre)) {
+              return movie.title.toLowerCase().includes(searchText.toLowerCase())
+                || movie.subtitle.toLowerCase().includes(searchText.toLowerCase())
+                || movie.storyline.toLowerCase().includes(searchText.toLowerCase());
+            }
+            return null;
+          },
+
+        ) }
+        bookmarkedOnly={ bookmarkedOnly }
+        selectedGenre={ selectedGenre }
+      />
+    </>
   );
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import PropTypes from 'prop-types';
 import React from 'react';
 
