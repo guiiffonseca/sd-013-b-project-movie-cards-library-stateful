d4b93407c7bb0cca00531df10a9f480eb1493747
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-07-30 15:53:15 -0300

    itens 1 ao 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,62 @@
 // implement SearchBar component here
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="incluiText">
+          Inclui o texto
+          <input
+            data-testid="text-input"
+            type="text"
+            name="incluiText"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label data-testid="checkbox-input-label" htmlFor="incluiText">
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            name="incluiFavoritos"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <label data-testid="select-input-label" htmlFor="seleciona">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            name="seleciona"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+
+};
+
+export default SearchBar;
