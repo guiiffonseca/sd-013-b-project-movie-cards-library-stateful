455ed444570324c8ac971e09e49a6d7f1aeacbb6
Jonmorais <jonathan_ms15@hotmail.com>
2021-07-30 17:21:52 -0300

    Requisito 15

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import MovieList from './components/MovieList';
-import movies from './data';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+// import Header from './components/Header';
+// import MovieList from './components/MovieList';
+// import movies from './data';
+// import SearchBar from './components/SearchBar';
+// import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header />
       <SearchBar />
-      <MovieList movies={ movies } />
-      <AddMovie onClick={ () => {} } />
+      <MovieList movies={ movies } /> */}
+      <MovieLibrary />
+      {/* <AddMovie onClick={ () => {} } /> */}
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,31 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieLibrary;
