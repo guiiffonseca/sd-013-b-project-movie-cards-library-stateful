46f0b0828253f525dfd3eba007b69c8cd272a0cc
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-29 21:12:44 -0300

    req 15 16 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
-// implement MovieLibrary component here
+import React, { useState } from 'react';
+import SearchBar from './SearchBar';
+
+function MovieLibrary(props) {
+  const { movies } = props;
+  const [searchText, setSearchText] = useState('');
+  const [bookmarkedOnly, setbookmarkedOnly] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState('');
+  return (
+    <SearchBar />
+  );
+}
+
+export default MovieLibrary;
