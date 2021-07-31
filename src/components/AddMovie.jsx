bf4a03323a811244f05bd5e74d58677503c1d19f
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-07-31 15:53:02 -0300

    itens 6 ao 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,18 @@
 // implement AddMovie component here
+
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title">
+          Título
+          <input type="text" data-testid="title-input" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
