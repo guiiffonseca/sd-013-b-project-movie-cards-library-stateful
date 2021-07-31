50fac6a2c4fd821d1ae03de69d6442cc72d5ab0f
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-07-31 11:16:36 -0300

    desafio 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
-// import MovieList from './MovieList';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
@@ -8,6 +8,22 @@ class MovieLibrary extends Component {
   constructor(props) {
     super(props);
     this.onClick = this.onClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    // const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnl: false,
+      selectedGenre: '',
+      // movies: movies,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
   }
 
   onClick() {
@@ -15,22 +31,37 @@ class MovieLibrary extends Component {
   }
 
   render() {
+    const { searchText, bookmarkedOnl, selectedGenre } = this.state;
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar
-          searchText=""
+          handleChange={ this.handleChange }
+          searchText={ searchText }
           onSearchTextChange={ () => 'callback' }
-          bookmarkedOnl="true"
+          bookmarkedOnl={ bookmarkedOnl }
           onBookmarkedChange={ () => 'callback' }
-          selectedGenre=""
+          selectedGenre={ selectedGenre }
           onSelectedGenreChange={ () => 'callback' }
         />
-        {/* <MovieList movies={ this.props.movies } /> */}
+        <MovieList movies={ movies } />
         <AddMovie onClick={ this.onClick } />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 class SearchBar extends Component {
   render() {
     const {
+      // handleChange,
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
