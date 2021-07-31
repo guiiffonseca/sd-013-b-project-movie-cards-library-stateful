c160f4670fa2f5b254504fdb96522334241c95e2
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-07-31 18:33:06 -0300

    item 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,9 +6,13 @@ class AddMovie extends React.Component {
   render() {
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="title">
+        <label data-testid="title-input-label" htmlFor="title-input">
           Título
           <input type="text" data-testid="title-input" />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+          Subtítulo
+          <input type="text" data-testid="subtitle-input" />
         </label>
       </form>
     );
