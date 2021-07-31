323217f82880355ad2483b017bdfe35270609c80
Guilherme Viana <guilhermehviana01@gmail.com>
2021-07-31 20:23:22 -0300

    Adiciona id para label do input tipo texto de AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 export default class AddMovie extends Component {
   render() {
     return (
-      <form htmlFor="true" data-testid="add-movie-form" />
-    // </form>
+      <form htmlFor="true" data-testid="add-movie-form">
+        <label htmlFor="true" data-testid="title-input-label">
+          Título
+          <input type="text" />
+        </label>
+      </form>
     );
   }
 }
