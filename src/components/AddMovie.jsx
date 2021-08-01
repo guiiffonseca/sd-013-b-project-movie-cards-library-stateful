0240e29b899b399f7ae23e119c4df093b3be0047
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-08-01 12:45:30 -0300

    item 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,7 +3,24 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      estadoNumber: '0',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      estadoNumber: event.target.value,
+    });
+  }
+
   render() {
+    const { estadoNumber } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title-input">
@@ -18,9 +35,18 @@ class AddMovie extends React.Component {
           Imagem
           <input type="text" data-testid="image-input" />
         </label>
+        <label data-testid="storyline-input-label" htmlFor="textarea">
+          Sinopse
+          <textarea type="text" data-testid="textarea" />
+        </label>
         <label data-testid="rating-input-label" htmlFor="title">
           Avaliação
-          <input type="number" data-testid="rating-input" />
+          <input
+            type="number"
+            data-testid="rating-input"
+            value={ estadoNumber }
+            onChange={ this.handleChange }
+          />
         </label>
       </form>
     );
