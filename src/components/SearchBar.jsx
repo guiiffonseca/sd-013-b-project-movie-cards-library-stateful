a3ea25e9c33df3ce44a16d113e37927f86465773
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-08-01 04:08:07 -0300

    requisito 5, proptypes prontos porem comentados para o component SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -36,6 +36,20 @@ class SearchBar extends React.Component {
             onChange={ onBookmarkedChange }
           />
         </label>
+        <label htmlFor="select-drop" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="select-drop"
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
