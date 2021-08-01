c7654e9db7fe4b21b131e33b5f791c27f8fe2747
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-08-01 05:30:59 -0300

    Requisitos 9 e 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       subtitle: '',
       title: '',
       imagePath: '',
@@ -15,20 +15,47 @@ class AddMovie extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({ title: event.target.value });
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
+        <label htmlFor="title" data-testid="title-input-label">
           Título
           <input
             type="text"
-            id="title-input"
+            name="title"
+            id="title"
             value={ title }
             data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label
+          htmlFor="subtitle"
+          data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            name="subtitle"
+            id="subtitle"
+            type="text"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="imagePath" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            name="imagePath"
+            id="imagePath"
+            value={ imagePath }
+            data-testid="image-input"
             onChange={ this.handleChange }
           />
         </label>
