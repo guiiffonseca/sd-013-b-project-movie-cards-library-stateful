8e176210026d72765993486c02e8efe3ebce7660
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-01 05:22:50 -0300

    #VQV

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        // searchText={string}
+        // onSeachTextChange={callback}
+        // bookmarkedOnly={boolean}
+        // onBokkmarkedChange={string}
+        // selectedGenre={string}
+        // onSelectedGenreChange={callback}
+      />
     </div>
   );
 }
