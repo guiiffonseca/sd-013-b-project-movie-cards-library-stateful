fd41db81b1c8997291f95edc4f53513aa1794f46
Isaddora Bayma <isadora.bayma@gmail.com>
2021-08-02 12:15:29 -0300

    requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,27 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+    return (
+      <h1>xxx</h1>
+    );
+  }
+}
+
+SearchBar.PropTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.callback,
+  bookmarkedOnly: PropTypes.boolean,
+  onBookmarkedChange: PropTypes.callbaccallback,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.callback,
+}
