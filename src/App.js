6d991206fd7ddafbe6d003061dc1c319f98df5fc
Dororops <leeurw@hotmail.com>
2021-08-02 02:31:12 -0300

    Adicionando ate Desafio-3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
+
 import './App.css';
+import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <SearchBar />
+      
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,40 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <form action="" data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+        Inclui o texto:
+          <input type="text" value="searchText" onChange="onSearchTextChange" data-testid="text-input"/>
+        </label>
+
+      </form>
+    );
+  }
+}
+
+// SearchBar.propTypes = {
+//   searchText: PropTypes.string.isRequired,
+//   onSearchTextChange: PropTypes.func.isRequired,
+//   bookmarkedOnly: PropTypes.bool.isRequired,
+//   onBookmarkedChange: PropTypes.func.isRequired,
+//   selectedGenre: PropTypes.string.isRequired,
+//   onSelectedGenreChange: PropTypes.func.isRequired,
+// };
+
+export default SearchBar;
