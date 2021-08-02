653e29a18b98af8c00d70c9844a3a73cc3677087
Thiago <thiagodesouza.dev@gmail.com>
2021-08-02 19:44:07 -0300

    requisitos 6 e 7

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+import AddMovie from './AddMovie';
+
+export default class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <AddMovie />
+      </div>
+    );
+  }
+}
