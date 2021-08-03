3a11c41b0acd1289da3130b1c8b9d954fe617549
Thiago <thiagodesouza.dev@gmail.com>
2021-08-03 10:28:20 -0300

    fix lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,23 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
 import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
 
 export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: 
+    };
+  }
   render() {
     return (
       <div>
         <AddMovie />
+        <SearchBar />
       </div>
     );
   }
