486b3f26c6483b66912f9473b31dace761aac833
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-03 18:19:25 -0300

    Campo de seleção de gênero renderizado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,65 +6,68 @@ class AddMovie extends React.Component {
       <div>
         <form data-testid="add-movie-form">
           <label
-            htmlFor="add-movie-title"
             data-testid="title-input-label"
           >
             Título:
             <input
-              id="add-movie-title"
               className="search-field"
               data-testid="title-input"
               type="text"
             />
           </label>
           <label
-            htmlFor="add-movie-subtitle"
             data-testid="subtitle-input-label"
           >
             Subtítulo:
             <input
-              id="add-movie-subtitle"
               className="search-field"
               data-testid="subtitle-input"
               type="text"
             />
           </label>
           <label
-            htmlFor="add-movie-image"
             data-testid="image-input-label"
           >
             Imagem:
             <input
-              id="add-movie-image"
               className="search-field"
               data-testid="image-input"
               type="text"
             />
           </label>
           <label
-            htmlFor="add-movie-storyline"
             data-testid="storyline-input-label"
           >
             Sinopse:
             <input
-              id="add-movie-storyline"
               className="search-field"
               data-testid="storyline-input"
               type="textarea"
             />
           </label>
           <label
-            htmlFor="add-movie-rating"
             data-testid="rating-input-label"
           >
             Avaliação:
             <input
-              id="add-movie-rating"
               className="search-field"
               data-testid="rating-input"
               type="number"
+            />
+          </label>
+          <label
+            data-testid="genre-input-label"
+          >
+            Gênero:
+            <select
+              className="search-field"
+              data-testid="genre-input"
+              name="genre"
             >
-            </input>
+              <option data-testid="genre-option" value="action">Ação</option>
+              <option data-testid="genre-option" value="comedy">Comédia</option>
+              <option data-testid="genre-option" value="thriller">Suspense</option>
+            </select>
           </label>
         </form>
       </div>
