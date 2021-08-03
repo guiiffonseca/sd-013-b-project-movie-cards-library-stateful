e1b5e4973ec771e0c0833a16b43c36102a814e74
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-03 17:16:09 -0300

    Cria o componente AddMovie e seus estados

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  render() {
+    const { onClick } = this.props;
+    return ();
+  }
+}
+
+export default AddMovie;
