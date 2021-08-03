f3ec3dadde1839336553a6fad3c775162edca904
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-03 17:38:22 -0300

    Campo de sinopse renderizado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -41,6 +41,18 @@ class AddMovie extends React.Component {
               type="text"
             />
           </label>
+          <label
+            htmlFor="add-movie-storyline"
+            data-testid="storyline-input-label"
+          >
+            Sinopse:
+            <input
+              id="add-movie-storyline"
+              className="search-field"
+              data-testid="storyline-input"
+              type="textarea"
+            />
+          </label>
         </form>
       </div>
     );
