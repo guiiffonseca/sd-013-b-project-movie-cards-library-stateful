64c5c73656b3e368929e00b6b291aca7101795d4
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-03 14:40:35 -0300

    Cria o coponente SearchBar, implementa suas props e inicia um form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,35 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkOnly,
+      onBookmarkChange,
+      selectGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkOnly: PropTypes.bool.isRequired,
+  onBookmarkChange: PropTypes.func.isRequired,
+  selectGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
