d337a5195eb93f18ebceab5661b8c78fa949605b
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-03 17:11:11 -0300

    Input de texto renderizado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,7 +5,17 @@ class AddMovie extends React.Component {
     return (
       <div>
         <form data-testid="add-movie-form">
-          <label>
+          <label
+            htmlFor="add-movie"
+            data-testid="title-input-label"
+          >
+            Título:
+            <input
+              id="add-movie"
+              className="search-field"
+              data-testid="title-input"
+              type="text"
+            />
           </label>
         </form>
       </div>
