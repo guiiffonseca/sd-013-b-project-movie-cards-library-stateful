7da15f46312818edc99fd3b403a89a96bb4afab9
YasminAraujo3 <yasmin.nunesaraujo@gmail.com>
2021-08-03 18:44:11 -0300

    Requisito 8 feito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,11 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+
+    this.onNewTitle = this.onNewTitle.bind(this);
+
+  }
+
+  onNewTitle(event) {
+    console.log(event.target.value);
+    this.setState({ title: event.target.value });
+  }
+
   render() {
     const { callBack } = this.props;
+    const { title } = this.state;
     return (
       <form>
-        <input type="text" onClick={ callBack } />
+        <label htmlFor="newTitle" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            onChange={ this.onNewTitle }
+            data-testid="title-input"
+            id="newTitle"
+          />
+        </label>
+        { console.log(callBack) }
       </form>
     );
   }
