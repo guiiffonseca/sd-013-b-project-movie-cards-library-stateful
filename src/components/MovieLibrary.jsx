2302866c4d925aee5e5b86bde502da5f1965aad6
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-08-04 00:21:23 -0300

    item 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,10 +2,29 @@
 
 import React from 'react';
 import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+
+    };
+  }
+
   render() {
-    return <AddMovie />;
+    const { searchText } = this.state;
+    return (
+      <div>
+        <SearchBar
+          value={ searchText }
+
+        />
+        <AddMovie />
+      </div>
+    );
   }
 }
 
