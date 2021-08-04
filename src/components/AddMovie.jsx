c375ccffbd7670981f60503f1e7811b743fc998e
ViniciusMossi <vini_agg@hotmail.com>
2021-08-04 10:28:21 -0400

    verifica requisito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React from 'react';
+
+const ESTADO_INICIAL = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = ESTADO_INICIAL;
+  }
+
+  render() {
+    return (
+      <main>
+        <form> </form>
+      </main>
+    );
+  }
+}
+
+export default AddMovie;
