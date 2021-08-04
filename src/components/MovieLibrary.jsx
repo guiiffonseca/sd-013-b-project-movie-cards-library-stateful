381483ee7987ea9fefd67eba96aa93ab444078ce
Thaís Sampaio <thais_castrocs@outlook.com>
2021-08-04 10:19:36 -0300

    Corrigindo erro lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,27 +1,33 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 export default class MovieLibrary extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchText: '',
+      /* searchText: '',
       bookmarkedOnly: false,
-      selectedGenre: '',
+      selectedGenre: '', */
       movies: props.movies,
 
     };
   }
 
   render() {
-    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { movies /* searchText, bookmarkedOnly, selectedGenre */ } = this.state;
     return (
       <div>
         <SearchBar />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
