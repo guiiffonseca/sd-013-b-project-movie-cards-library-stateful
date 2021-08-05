99f3f7a981a1c3afa300acd2c5f738d387975d42
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-08-05 18:03:52 -0300

    agora vai

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import movies from '../data';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
