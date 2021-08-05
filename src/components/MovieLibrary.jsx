72119989dce1eda3f3262647f39035988680b349
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-08-05 17:49:56 -0300

    requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -19,6 +19,7 @@ class MovieLibrary extends Component {
   render() {
     const { movies } = this.props;
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    console.log(searchText, bookmarkedOnly, selectedGenre);
     return (
       <div>
         <h2> My awesome movie library </h2>
