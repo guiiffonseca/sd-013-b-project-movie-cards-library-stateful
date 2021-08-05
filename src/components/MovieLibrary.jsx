e335d4031032a82f4241dbcd74cee29500d7733e
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-08-05 17:00:32 -0300

    ainda nao

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+import MovieCard from './MovieCard';
 
 class MovieLibrary extends Component {
   constructor(props) {
@@ -23,7 +24,8 @@ class MovieLibrary extends Component {
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ movies } />
+        {movies.map((value, index) => <MovieCard key={ index } movie={ value } />)};
+        <MovieList />
         <AddMovie />
       </div>
     );
