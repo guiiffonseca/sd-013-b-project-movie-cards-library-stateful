d558880231caf7ac45bb08946b08c0f9b7371cd5
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-08-05 17:42:50 -0300

    requisito 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import movies from '../data';
+// import movies from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div data-testid="movie-list" className="movie-list">
         { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
