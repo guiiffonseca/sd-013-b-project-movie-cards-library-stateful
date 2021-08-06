12a5632a0aebb3784fd300ffad670de42807f6e6
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-05 21:35:52 -0300

    Componente AddTitle corretamente implementada

--- src/components/addMovie/AddTitle.jsx
+++ src/components/addMovie/AddTitle.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-class Title extends React.Component {
-  <p>oi</p>
+class AddTitle extends React.Component {
+  render() {
+    return (
+      <label
+        htmlFor="add-movie-title"
+        data-testid="title-input-label"
+      >
+        Título:
+        <input
+          id="add-movie-title"
+          className="search-field"
+          data-testid="title-input"
+          type="text"
+        />
+      </label>
+    );
+  }
 }
+
+export default AddTitle;
