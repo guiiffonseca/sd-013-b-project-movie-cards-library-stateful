d8c2493fe2c678e23825a92b5f6b19a534a9a895
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-07 18:27:23 -0300

    AddMovie renderizado dentro de MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   render() {
     return (
-      <p>xablau</p>
+      <AddMovie />
     );
   }
 }
