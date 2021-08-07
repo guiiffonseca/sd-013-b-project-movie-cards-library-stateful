970ca00700604bd33bbe722d017bb824a6dcb8aa
gsdu-arte <gabi15duarte@hotmail.com>
2021-08-07 20:32:59 -0300

    Add SendButton

---
+++ src/components/SendButton.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SendButton extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+SendButton.propTypes = {
+
+};
+
+export default SendButton;
