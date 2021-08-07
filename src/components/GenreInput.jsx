d088f8abbb2985dd453191f0128a346d3cd1b3c9
gsdu-arte <gabi15duarte@hotmail.com>
2021-08-07 20:57:59 -0300

    Resoluçao do requisito 13

--- src/components/GenreInput.jsx
+++ src/components/GenreInput.jsx
@@ -3,14 +3,46 @@ import PropTypes from 'prop-types';
 
 class GenreInput extends Component {
   render() {
+    const { genre, handleChange } = this.props;
     return (
-
+      <label
+        htmlFor="genre"
+        data-testid="genre-input-label"
+      >
+        <select
+          name="genre"
+          id="genre"
+          data-testid="genre-input"
+          value={ genre }
+          onChange={ handleChange }
+        >
+          <option
+            data-testid="genre-option"
+            value="action"
+          >
+            Ação
+          </option>
+          <option
+            data-testid="genre-option"
+            value="comedy"
+          >
+            Comédia
+          </option>
+          <option
+            data-testid="genre-option"
+            value="thriller"
+          >
+            Suspense
+          </option>
+        </select>
+      </label>
     );
   }
 }
 
 GenreInput.propTypes = {
-
+  genre: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default GenreInput;
