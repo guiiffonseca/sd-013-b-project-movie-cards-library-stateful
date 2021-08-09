a0b2eb9d5b19e4f49d8cc2d979cbfbe9c5e6e90c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-08 23:48:16 -0300

    Componente App passada de função para classe. Implementação anterior comentada.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <SearchBar
-        searchText="My Text"
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <SearchBar
+//         searchText=""
+//         onSearchTextChange={ movies }
+//         bookmarkedOnly={ false }
+//         onBookmarkedChange={ movies }
+//         selectedGenre={ movies }
+//         onSelectedGenreChange={ movies }
+//       />
+//       <MovieLibrary />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+        searchText=""
         onSearchTextChange={ movies }
         bookmarkedOnly={ false }
         onBookmarkedChange={ movies }
         selectedGenre={ movies }
         onSelectedGenreChange={ movies }
-      />
-    </div>
-  );
+        />
+        <MovieLibrary />
+      </div>
+    );
+  }
 }
 
 export default App;
