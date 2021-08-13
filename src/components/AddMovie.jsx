6785c895d231df609737af37dd92d22d937a36a8
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-13 13:46:36 -0300

    Retirada propriedade movieRating. Adicionada função onClick como props. Função onClick substitui submitMovie. Componente AddRating colocada embaixo de AddGenre.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import AddTitle from './addMovie/AddTitle';
 import AddSubtitle from './addMovie/AddSubtitle';
 import AddImage from './addMovie/AddImage';
@@ -9,25 +8,43 @@ import AddRating from './addMovie/AddRating';
 import AddGenre from './addMovie/AddGenre';
 
 class AddMovie extends React.Component {
-  submitMovie(event) {
-    console.log(event);
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      subtitle: '',
+      storyline: '',
+      imagePath: '',
+      bookmarked: false,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
   }
 
   render() {
-    const { movieRating } = this.props;
+    const {
+      title, subtitle, storyline,
+      imagePath, bookmarked, genre,
+    } = this.state;
+    const { onClick } = this.props;
     return (
       <div className="search-field">
         <form data-testid="add-movie-form">
-          <AddTitle />
-          <AddSubtitle />
-          <AddImage />
-          <AddStoryline />
-          <AddRating movieRating={ movieRating } />
-          <AddGenre />
+          <AddTitle title={ title } onChange={ this.handleChange } />
+          <AddSubtitle subtitle={ subtitle } onChange={ this.handleChange } />
+          <AddImage imagePath={ imagePath } onChange={ this.handleChange } />
+          <AddStoryline storyline={ storyline } onChange={ this.handleChange } />
+          <AddGenre genre={ genre } onChange={ this.handleChange } />
+          <AddRating />
           <button
             data-testid="send-button"
             type="button"
-            onClick={ this.submitMovie }
+            onClick={ onClick }
           >
             Adicionar filme
           </button>
@@ -38,7 +55,7 @@ class AddMovie extends React.Component {
 }
 
 AddMovie.propTypes = {
-  movieRating: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default AddMovie;
