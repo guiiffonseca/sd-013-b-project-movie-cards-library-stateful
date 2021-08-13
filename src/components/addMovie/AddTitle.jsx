888941292925f65580eeacbe54db111cdc8de84c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-13 11:40:39 -0300

    Adicionada propriedade name no componente AddTitle. Recebe os valores title e a callback onChange como props.

--- src/components/addMovie/AddTitle.jsx
+++ src/components/addMovie/AddTitle.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddTitle extends React.Component {
   render() {
+    const { title, onChange } = this.props;
     return (
       <label
         htmlFor="add-movie-title"
@@ -13,10 +15,18 @@ class AddTitle extends React.Component {
           className="search-field"
           data-testid="title-input"
           type="text"
+          name="title"
+          value={ title }
+          onChange={ onChange }
         />
       </label>
     );
   }
 }
+
+AddTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default AddTitle;
