5f426b128c8002c594aaba8480eb2e47b71515c4
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-13 11:41:52 -0300

    Recebe os valores genre e a callback onChange como props.

--- src/components/addMovie/AddGenre.jsx
+++ src/components/addMovie/AddGenre.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddGenre extends React.Component {
   render() {
+    const { genre, onChange } = this.props;
     return (
       <label
         htmlFor="add-movie-genre"
@@ -13,6 +15,8 @@ class AddGenre extends React.Component {
           className="search-field"
           data-testid="genre-input"
           name="genre"
+          value={ genre }
+          onChange={ onChange }
         >
           <option data-testid="genre-option" value="action">Ação</option>
           <option data-testid="genre-option" value="comedy">Comédia</option>
@@ -22,5 +26,10 @@ class AddGenre extends React.Component {
     );
   }
 }
+
+AddGenre.propTypes = {
+  genre: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default AddGenre;
