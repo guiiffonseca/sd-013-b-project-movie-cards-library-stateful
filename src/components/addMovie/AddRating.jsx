abcb85abbcab5e0dfd23c675f382c45d77617f0f
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-13 09:33:07 -0300

    Estado inicial implementado com sucesso. Função para atualizar o valor de rating implementada. Retiradas as props.

--- src/components/addMovie/AddRating.jsx
+++ src/components/addMovie/AddRating.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 class AddRating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieRating: 0,
+    };
+
+    this.onRatingChange = this.onRatingChange.bind(this);
+  }
+
+  onRatingChange(event) {
+    this.setState({ movieRating: event.target.value });
+  }
+
   render() {
+    const { movieRating } = this.state;
     return (
       <label
         htmlFor="add-movie-rating"
@@ -13,6 +27,11 @@ class AddRating extends React.Component {
           className="search-field"
           data-testid="rating-input"
           type="number"
+          min="0"
+          max="5"
+          step="0.1"
+          value={ movieRating }
+          onChange={ this.onRatingChange }
         />
       </label>
     );
