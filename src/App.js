fe59318068d4eaefdbc38eb2057af88846cdcb1d
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-08-21 18:43:58 -0300

    Requirement 1 complete.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import MovieList from './components/MovieList';
 import movies from './data';
 
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
       <MovieList movies={ movies } />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="search-bar">
+        <h1>Search Bar</h1>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired, // uma string
+  onSearchTextChange: PropTypes.func.isRequired, // uma callback
+  bookmarkedOnly: PropTypes.bool.isRequired, // um boolean
+  onBookmarkedChange: PropTypes.func.isRequired, // uma callback
+  selectedGenre: PropTypes.string.isRequired, // uma string
+  onSelectedGenreChange: PropTypes.func.isRequired, // uma callback
+};
+
+export default SearchBar;
