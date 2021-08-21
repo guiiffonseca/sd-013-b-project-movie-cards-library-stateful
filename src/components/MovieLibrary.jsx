cea963f9d7f2cfcacbb0b37872e0003c988f1e8e
Itaopara Neto <itaopara@gmail.com>
2021-08-21 19:50:28 -0300

    qse la

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,72 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes, { bool } from 'prop-types';
+import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    // Valeu Alberto! https://app.betrybe.com/course/front-end/componentes-com-estado-eventos-e-formularios-com-react/formularios-no-react/537fc0f4-1be1-4cd8-8333-9432fa722672/conteudos/815da631-cbdb-4637-b7fb-17e29240a8d1/event-handlers-genericos/20a033ed-ccfc-4e65-b6cb-53abf58039bb?use_case=side_bar
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+    this.setState({
+      [name]: value,
+    });
+    if (bookmarkedOnly === true) {
+      this.setState({
+        movies: movies.filter((movie) => movie.bookmarkedOnly === true),
+      });
+    } else if (selectedGenre !== '') {
+      this.setState({
+        movies: movies.filter((movie) => movie.genre === selectedGenre),
+      });
+    } else if (searchText !== '') {
+      this.setState({
+        movies: movies.filter((movie) => movie.title.includes(searchText)
+        || movie.subtitle.includes(searchText) || movie.storyline.includes(searchText)),
+      });
+    }
+  }
+
+  // Como texto do readme está confuso olhei o PR :https://github.com/tryber/sd-013-b-project-movie-cards-library-stateful/pull/109/files para entender o que fazer;
+  // Toda logica desenvolvida foi feita por mim!
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={ this.searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ this.bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ this.selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ movies } />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf.isRequired,
+};
