6c36e4d0233f812f4f4c80ea74ff2c5b97882123
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-08-21 19:57:26 -0300

    Requirement 6 completed.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,16 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <h1>Add Movie</h1>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
