ecdaae9cd74fb73a8864b86cac9a3062296bfa4d
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-08-31 10:23:15 -0300

    req 19 broken

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 export default class MovieLibrary extends Component {
   constructor(props) {
     super();
-    this.updateMovie = this.updateMovie.bind(this);
+    this.changeMovie = this.changeMovie.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
 
     this.state = {
       searchText: '',
@@ -16,8 +20,39 @@ export default class MovieLibrary extends Component {
     };
   }
 
-  updateMovie(newMovie) {
+  onSearchTextChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  onBookmarkedChange(event) {
+    this.setState({ bookmarkedOnly: event.target.checked });
+  }
+
+  onSelectedGenreChange(event) {
+    this.setState({ selectedGenre: event.target.value });
+  }
+
+  changeMovie(newMovie) {
     this.setState((prevstate) => ({ movieList: [...prevstate.movieList, newMovie] }));
+  }
+
+  searchMovies() {
+    const { searchText, bookmarkedOnly, selectedGenre, movieList } = this.state;
+    const searchByText = movieList.filter((movie) => movie.title.includes(searchText)
+    || movie.subtitle.includes(searchText) || movie.storyline.includes(searchText));
+    const searchByBookmark = movieList.filter((movie) => movie.bookmarked === true);
+    const searchByGenre = movieList.filter((movie) => movie.genre === selectedGenre);
+
+    if (searchText !== '') {
+      return searchByText;
+    }
+    if (bookmarkedOnly === true) {
+      return searchByBookmark;
+    }
+    if (selectedGenre !== '') {
+      return searchByGenre;
+    }
+    return movieList;
   }
 
   render() {
@@ -28,8 +63,12 @@ export default class MovieLibrary extends Component {
           searchText={ searchText }
           bookmarkedOnly={ bookmarkedOnly }
           selectedGenre={ selectedGenre }
+          onSearchTextChange={ this.onSearchTextChange }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
         />
-        <AddMovie onclick={ this.updateMovie } />
+        <MovieList movies={ this.searchMovies() } />
+        <AddMovie onclick={ this.changeMovie } />
       </div>
     );
   }
